d86d74847e59f3dfdc592cc887a9666105e45a03
Stanley Huang <stanleyhuangyc@gmail.com>
2014-08-18 21:42:59 +1000

    Fix response data dropping issue

--- firmware_v2/elm327emu/elm327emu.ino
+++ firmware_v2/elm327emu/elm327emu.ino
@@ -3,15 +3,41 @@
 
 SoftwareSerial SerialBT(A2, A3);
 
-void setup() {
+void setup()
+{
   Serial.begin(38400);
   SerialBT.begin(STREAM_BAUDRATE);
 }
 
-void loop() {
-  if (Serial.available())
-    SerialBT.write(Serial.read());
-  
-  if (SerialBT.available())
-    Serial.write(SerialBT.read());
+char cmd[32];
+byte cmdlen = 0;
+byte sent = 0;
+
+void loop()
+{
+  if (Serial.available()) {
+    char c = Serial.read();
+    SerialBT.write(c);
+    if (++sent == 20 || c == '\r') {
+      delay(20);
+      sent = 0;
+    } else {
+      delay(5);
+    }
+  }
+
+  if (SerialBT.available()) {
+    if (cmdlen < sizeof(cmd)) {
+      char c = SerialBT.read();
+      if (c == '\r' || c == '\n') {
+        if (cmdlen > 0) {
+          Serial.write(cmd, cmdlen);
+          Serial.write('\r');
+          cmdlen = 0;
+        }
+      } else {
+        cmd[cmdlen++] = SerialBT.read();
+      }
+    }
+  }
 }
