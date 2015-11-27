27c8b1f5127a0265faf3b0e394d5e0fe2c3d8e49
Stanley Huang <stanleyhuangyc@gmail.com>
2015-11-27 23:45:27 +1100

    Experimental sketch for Freematics ONE

---
+++ firmware_v4/datalogger/datalogger.h
@@ -0,0 +1,257 @@
+/*************************************************************************
+* Telematics Data Logger Class
+* Distributed under GPL v2.0
+* Written by Stanley Huang <stanleyhuangyc@gmail.com>
+* Visit http://freematics.com for more information
+*************************************************************************/
+
+#define FORMAT_BIN 0
+#define FORMAT_TEXT 1
+
+typedef struct {
+    uint32_t time;
+    uint16_t pid;
+    uint8_t flags;
+    uint8_t checksum;
+    float value[3];
+} LOG_DATA_COMM;
+
+#define PID_GPS_LATITUDE 0xA
+#define PID_GPS_LONGITUDE 0xB
+#define PID_GPS_ALTITUDE 0xC
+#define PID_GPS_SPEED 0xD
+#define PID_GPS_HEADING 0xE
+#define PID_GPS_SAT_COUNT 0xF
+#define PID_GPS_TIME 0x10
+#define PID_GPS_DATE 0x11
+
+#define PID_ACC 0x20
+#define PID_GYRO 0x21
+#define PID_COMPASS 0x22
+#define PID_MEMS_TEMP 0x23
+#define PID_BATTERY_VOLTAGE 0x24
+
+#define PID_DATA_SIZE 0x80
+
+#define FILE_NAME_FORMAT "/DAT%05d.CSV"
+
+#if ENABLE_DATA_OUT
+
+#if USE_SOFTSERIAL
+SoftwareSerial SerialRF(A2, A3);
+#elif defined(RF_SERIAL)
+#define SerialRF RF_SERIAL
+#else
+#define SerialRF Serial
+#endif
+
+#endif
+
+#if ENABLE_DATA_LOG
+static File sdfile;
+#endif
+
+typedef struct {
+    uint8_t pid;
+    char name[3];
+} PID_NAME;
+
+const PID_NAME pidNames[] PROGMEM = {
+{PID_ACC, {'A','C','C'}},
+{PID_GYRO, {'G','Y','R'}},
+{PID_COMPASS, {'M','A','G'}},
+{PID_GPS_LATITUDE, {'L','A','T'}},
+{PID_GPS_LONGITUDE, {'L','O','N'}},
+{PID_GPS_ALTITUDE, {'A','L','T'}},
+{PID_GPS_SPEED, {'S','P','D'}},
+{PID_GPS_HEADING, {'C','R','S'}},
+{PID_GPS_SAT_COUNT, {'S','A','T'}},
+{PID_GPS_TIME, {'T','I','M'}},
+{PID_GPS_DATE, {'D','T','E'}},
+{PID_BATTERY_VOLTAGE, {'B','A','T'}},
+{PID_DATA_SIZE, {'D','A','T'}},
+};
+
+class CDataLogger {
+public:
+    void initSender()
+    {
+#if ENABLE_DATA_OUT
+        SerialRF.begin(STREAM_BAUDRATE);
+        SerialRF.print("AT+NAMEFreematics");
+        delay(10);
+#endif
+#if ENABLE_DATA_LOG
+        m_lastDataTime = 0;
+#endif
+    }
+    void recordData(const char* buf, byte len)
+    {
+#if ENABLE_DATA_LOG
+        dataSize += sdfile.print(dataTime - m_lastDataTime);
+        sdfile.write(',');
+        dataSize += sdfile.write(buf, len);
+        sdfile.write("\r\n");
+        dataSize += 3;
+        m_lastDataTime = dataTime;
+#endif
+    }
+    void sendData(const char* buf, byte len)
+    {
+#if ENABLE_DATA_OUT
+        SerialRF.write(buf, len);
+        SerialRF.write('\r');
+        delay(10);
+#endif
+    }
+    void logData(char c)
+    {
+#if ENABLE_DATA_LOG
+        if (c >= ' ') {
+            sdfile.write(c);
+            dataSize++;
+        }
+#endif
+    }
+    void logData(uint16_t pid)
+    {
+        char buf[8];
+        byte len = translatePIDName(pid, buf);
+#if ENABLE_DATA_OUT
+#if STREAM_FORMAT != FORMAT_BIN
+        sendData(buf, len);
+#endif
+#endif
+        recordData(buf, len);
+    }
+    void logData(uint16_t pid, int value)
+    {
+        char buf[16];
+        byte n = translatePIDName(pid, buf);
+        byte len = sprintf(buf + n, "%d", value) + n;
+#if ENABLE_DATA_OUT
+#if STREAM_FORMAT == FORMAT_BIN
+        LOG_DATA_COMM ld = {dataTime, pid, 1, 0, value};
+        ld.checksum = getChecksum((char*)&ld, 12);
+        sendData((const char*)&ld, 12);
+#else
+        sendData(buf, len);
+#endif
+#endif
+        recordData(buf, len);
+    }
+    void logData(uint16_t pid, int32_t value)
+    {
+        char buf[20];
+        byte n = translatePIDName(pid, buf);
+        byte len = sprintf(buf + n, "%ld", value) + n;
+#if ENABLE_DATA_OUT
+#if STREAM_FORMAT == FORMAT_BIN
+        LOG_DATA_COMM ld = {dataTime, pid, 1, 0, value};
+        ld.checksum = getChecksum((char*)&ld, 12);
+        sendData((const char*)&ld, 12);
+#else
+        sendData(buf, len);
+#endif
+#endif
+        recordData(buf, len);
+    }
+    void logData(uint16_t pid, uint32_t value)
+    {
+        char buf[20];
+        byte n = translatePIDName(pid, buf);
+        byte len = sprintf(buf + n, "%lu", value) + n;
+#if ENABLE_DATA_OUT
+#if STREAM_FORMAT == FORMAT_BIN
+        LOG_DATA_COMM ld = {dataTime, pid, 1, 0, value};
+        ld.checksum = getChecksum((char*)&ld, 12);
+        sendData((const char*)&ld, 12);
+#else
+        sendData(buf, len);
+#endif
+#endif
+        recordData(buf, len);
+    }
+    void logData(uint16_t pid, int value1, int value2, int value3)
+    {
+        char buf[24];
+        byte n = translatePIDName(pid, buf);
+        byte len = sprintf(buf + n, "%d,%d,%d", value1, value2, value3) + n;
+#if ENABLE_DATA_OUT
+#if STREAM_FORMAT == FORMAT_BIN
+        LOG_DATA_COMM ld = {dataTime, pid, 3, 0, {value1, value2, value3}};
+        ld.checksum = getChecksum((char*)&ld, 20);
+        sendData((const char*)&ld, 20);
+#else
+        sendData(buf, len);
+#endif
+#endif
+        recordData(buf, len);
+    }
+#if ENABLE_DATA_LOG
+    uint16_t openFile(uint16_t logFlags = 0, uint32_t dateTime = 0)
+    {
+        uint16_t fileIndex;
+        char filename[24] = "/FRMATICS";
+
+        dataSize = 0;
+        if (SD.exists(filename)) {
+            for (fileIndex = 1; fileIndex; fileIndex++) {
+                sprintf(filename + 9, FILE_NAME_FORMAT, fileIndex);
+                if (!SD.exists(filename)) {
+                    break;
+                }
+            }
+            if (fileIndex == 0)
+                return 0;
+        } else {
+            SD.mkdir(filename);
+            fileIndex = 1;
+            sprintf(filename + 9, FILE_NAME_FORMAT, 1);
+        }
+
+        sdfile = SD.open(filename, FILE_WRITE);
+        if (!sdfile) {
+            return 0;
+        }
+        m_lastDataTime = dateTime;
+        return fileIndex;
+    }
+    void closeFile()
+    {
+        sdfile.close();
+    }
+    void flushFile()
+    {
+        sdfile.flush();
+    }
+#endif
+    uint32_t dataTime;
+    uint32_t dataSize;
+private:
+    byte getChecksum(char* buffer, byte len)
+    {
+        uint8_t checksum = 0;
+        for (byte i = 0; i < len; i++) {
+          checksum ^= buffer[i];
+        }
+        return checksum;
+    }
+    byte translatePIDName(uint16_t pid, char* text)
+    {
+#if STREAM_FORMAT == FORMAT_TEXT
+        for (uint16_t n = 0; n < sizeof(pidNames) / sizeof(pidNames[0]); n++) {
+            uint16_t id = pgm_read_byte(&pidNames[n].pid);
+            if (pid == id) {
+                memcpy_P(text, pidNames[n].name, 3);
+                text[3] = ',';
+                return 4;
+            }
+        }
+#endif
+        return sprintf(text, "%X,", pid);
+    }
+#if ENABLE_DATA_LOG
+    uint32_t m_lastDataTime;
+#endif
+};
---
+++ firmware_v4/datalogger/datalogger.ino
@@ -0,0 +1,440 @@
+/*************************************************************************
+* Arduino OBD-II/G-Force Data Logger and Transmitter
+* Distributed under GPL v2.0
+* Copyright (c) 2013-15 Stanley Huang <stanleyhuangyc@gmail.com>
+* All rights reserved.
+*************************************************************************/
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <I2Cdev.h>
+#include <MPU9150.h>
+#include <Narcoleptic.h>
+#include "config.h"
+#if ENABLE_DATA_LOG
+#include <SD.h>
+#endif
+#include "Freematics.h"
+#include "datalogger.h"
+
+// logger states
+#define STATE_SD_READY 0x1
+#define STATE_OBD_READY 0x2
+#define STATE_GPS_FOUND 0x4
+#define STATE_GPS_READY 0x8
+#define STATE_MEMS_READY 0x10
+#define STATE_SLEEPING 0x20
+
+#if VERBOSE && !ENABLE_DATA_OUT
+#define SerialInfo Serial
+#else
+#define SerialInfo SerialRF
+#endif
+
+void(* resetFunc) (void) = 0; //declare reset function at address 0
+
+static uint8_t lastFileSize = 0;
+static uint16_t fileIndex = 0;
+
+static const byte PROGMEM pidTier1[]= {PID_RPM, PID_SPEED, PID_ENGINE_LOAD, PID_THROTTLE};
+static const byte PROGMEM pidTier2[] = {PID_COOLANT_TEMP, PID_INTAKE_TEMP, PID_DISTANCE};
+
+#define TIER_NUM1 sizeof(pidTier1)
+#define TIER_NUM2 sizeof(pidTier2)
+
+#if USE_MPU6050 || USE_MPU9150
+MPU6050 accelgyro;
+#endif
+
+static const byte PROGMEM parts[][3] = {
+  {'S','D'},
+  {'A','C','C'},
+  {'O','B','D'},
+  {'G','P','S'},
+};
+
+typedef enum {
+  PART_SD = 0,
+  PART_MEMS,
+  PART_OBD,
+  PART_GPS,
+} PART_ID;
+
+class CLogger : public COBDSPI, public CDataLogger
+{
+public:
+    CLogger():state(0) {}
+    void showStatus(byte partID, bool OK)
+    {
+#if ENABLE_DATA_OUT
+      char buf[4];
+      memcpy_P(buf, parts[partID], 3);
+      buf[3] = 0;
+      SerialRF.print(buf);
+      SerialRF.print(' ');
+      SerialRF.println(OK ? "OK" : "NO");
+#endif
+    }
+    void setup()
+    {
+        bool success;
+        state = 0;
+        
+#if ENABLE_DATA_LOG
+        success = initSD() && openLogFile();
+        showStatus(PART_SD, success);
+#endif
+
+#if USE_MPU6050 || USE_MPU9150
+        Wire.begin();
+        accelgyro.initialize();
+        success = accelgyro.testConnection();
+        if (success) {
+          state |= STATE_MEMS_READY;
+        }
+        showStatus(PART_MEMS, success);
+#endif
+
+#if OBD_UART_BAUDRATE
+        setBaudRate(OBD_UART_BAUDRATE);
+#endif
+
+#if USE_GPS
+        success = initGPS(GPS_SERIAL_BAUDRATE);
+        if (success) {
+            state |= STATE_GPS_FOUND;
+        }
+        showStatus(PART_GPS, success);
+#endif
+
+        success = init();
+        if (success) {
+            state |= STATE_OBD_READY;
+        }
+        showStatus(PART_OBD, success);
+    }
+#if USE_GPS
+    void logGPSData()
+    {
+#if LOG_GPS_NMEA_DATA
+        // issue the command to get NMEA data (one line per request)
+        char buf[128];
+        if (sendCommand("ATGRR\r", buf, sizeof(buf))) {
+            logData(buf + 4);
+        }
+#endif
+#if LOG_GPS_PARSED_DATA
+        // issue the command to get parsed GPS data
+        // the return data is in following format:
+        // $GPS,date,time,lat,lon,altitude,speed,course,sat
+        static GPS_DATA gd = {0};
+        byte mask = 0;
+        byte index = -1;
+        char buf[12];
+        byte n = 0;
+        write("ATGPS\r");
+        dataTime = millis();
+        for (uint32_t t = dataTime; millis() - t < GPS_DATA_TIMEOUT; ) {
+            if (!available()) continue;
+            char c = read();
+#if VERBOSE
+            logData(c);
+#endif
+            if (c != ',' && c != '>') {
+              if (n < sizeof(buf) - 1) buf[n++] = c;
+              continue;
+            }
+            buf[n] = 0;
+            if (index == -1) {
+                // need to verify header
+                if (strcmp(buf + n - 4, "$GPS") == 0) {
+                  index = 0;
+                }
+            } else {
+                long v = atol(buf);
+                switch (index) {
+                case 0:
+                    if (gd.date != v) {
+                      int year = v % 100;
+                      // filter out invalid date
+                      if (v < 1000000 && v >= 10000 && year >= 15 && (gd.date == 0 || year - (gd.date % 100) <= 1)) {
+                        gd.date = v;
+                        mask |= 0x1;
+                      }
+                    }
+                    break;
+                case 1:
+                    if (gd.time != v) {
+                      gd.time = v;
+                      mask |= 0x2;
+                    }                            
+                    break;
+                case 2:
+                    if (gd.lat != v) {
+                      gd.lat = v;
+                      mask |= 0x4;
+                    }
+                    break;
+                case 3:
+                    if (gd.lon != v) {
+                      gd.lon = v;
+                      mask |= 0x8;
+                    }
+                    break;
+                case 4:
+                    if (gd.alt != (int)v) {
+                      gd.alt = (int)v;
+                      mask |= 0x10;
+                    }
+                    break;
+                case 5:
+                    if (gd.speed != (byte)v) {
+                      gd.speed = (byte)v;
+                      mask |= 0x20;
+                    }
+                    break;
+                case 6:
+                    if (gd.heading != (int)v) {
+                      gd.heading = (int)v;
+                      mask |= 0x40;
+                    }
+                    break;
+                case 7:
+                    if (gd.sat != (byte)v) {
+                      gd.sat = (byte)v;
+                      mask |= 0x80;
+                    }
+                    break;
+                }
+                index++;
+            }
+            n = 0;
+            if (c == '>') {
+                // prompt char received, now process data
+                if (mask) {
+                  // something has changed
+                  if (mask & 0x1) logData(PID_GPS_DATE, gd.date);
+                  if (mask & 0x2) logData(PID_GPS_TIME, gd.time);
+                  if (mask & 0x4) logData(PID_GPS_LATITUDE, gd.lat);
+                  if (mask & 0x8) logData(PID_GPS_LONGITUDE, gd.lon);
+                  if (mask & 0x10) logData(PID_GPS_ALTITUDE, gd.alt);
+                  if (mask & 0x20) logData(PID_GPS_SPEED, gd.speed);
+                  if (mask & 0x40) logData(PID_GPS_HEADING, gd.heading);
+                  if (mask & 0x80) logData(PID_GPS_SAT_COUNT, gd.sat);
+                }                        
+                // discard following data if any
+                while (available()) read();
+                break;
+            }
+        }
+#endif
+    }
+#endif
+#if USE_MPU6050 || USE_MPU9150
+    void logMEMSData()
+    {
+        if (!(state & STATE_MEMS_READY))
+            return;
+
+#if USE_MPU9150
+        int16_t mems[3][3] = {0};
+        int16_t mx, my, mz;
+        accelgyro.getMotion9(&mems[0][0], &mems[0][1], &mems[0][2], &mems[1][0], &mems[1][1], &mems[1][2], &mems[2][0], &mems[2][1], &mems[2][2]);
+        dataTime = millis();
+        // assume PID_GYRO = PID_AAC + 1, PID_MAG = PID_AAC + 2
+        for (byte n = 0; n < 3; n++) {
+          logData(PID_ACC + n, mems[n][0] >> 4, mems[n][1] >> 4, mems[n][2] >> 4);
+        }
+        
+#else
+        int16_t mems[2][3] = {0};
+        accelgyro.getMotion6(&mems[0][0], &mems[0][1], &mems[0][2], &mems[1][0], &mems[1][1], &mems[1][2]);
+        dataTime = millis();
+        for (byte n = 0; n < 2; n++) {
+          logData(PID_ACC + n, mems[n][0] >> 4, mems[n][1] >> 4, mems[n][2] >> 4);
+        }
+#endif
+    }
+#endif
+#if ENABLE_DATA_LOG
+    int openLogFile()
+    {
+        uint16_t index = openFile();
+        if (!index) {
+            delay(1000);
+            index = openFile();
+        }
+        if (index) {
+            if (sdfile.print("#FREEMATICS\r\n") > 0) {
+              state |= STATE_SD_READY;
+            } else {
+              index = 0;
+            }
+        }
+#if VERBOSE
+        SerialInfo.print("File ID: ");
+        SerialInfo.println(index);
+        delay(3000);
+#endif
+        return index;
+    }
+    bool initSD()
+    {
+        state &= ~STATE_SD_READY;
+        pinMode(SS, OUTPUT);
+        Sd2Card card;
+        if (card.init(SPI_HALF_SPEED, SD_CS_PIN)) {
+#if VERBOSE
+            const char* type;
+            switch(card.type()) {
+            case SD_CARD_TYPE_SD1:
+                type = "SD1";
+                break;
+            case SD_CARD_TYPE_SD2:
+                type = "SD2";
+                break;
+            case SD_CARD_TYPE_SDHC:
+                type = "SDHC";
+                break;
+            default:
+                type = "SDx";
+            }
+
+            SerialInfo.print("SD type: ");
+            SerialInfo.print(type);
+
+            SdVolume volume;
+            if (!volume.init(card)) {
+                SerialInfo.println(" No FAT!");
+                return false;
+            }
+
+            uint32_t volumesize = volume.blocksPerCluster();
+            volumesize >>= 1; // 512 bytes per block
+            volumesize *= volume.clusterCount();
+            volumesize >>= 10;
+            SerialInfo.print(" SD size: ");
+            SerialInfo.print((int)((volumesize + 511) / 1000));
+            SerialInfo.println("GB");
+            delay(3000);
+#endif
+        }
+
+        return SD.begin(SD_CS_PIN);
+    }
+    void flushData()
+    {
+        // flush SD data every 1KB
+        byte dataSizeKB = dataSize >> 10;
+        if (dataSizeKB != lastFileSize) {
+#if VERBOSE
+            // display logged data size
+            SerialInfo.print(dataSize);
+            SerialInfo.println(" bytes");
+#endif
+            flushFile();
+            lastFileSize = dataSizeKB;
+#if MAX_LOG_FILE_SIZE
+            if (dataSize >= 1024L * MAX_LOG_FILE_SIZE) {
+              closeFile();
+              if (openLogFile() == 0) {
+                  state &= ~STATE_SD_READY;
+              }
+            }
+#endif
+        }
+    }
+#endif
+    void reconnect()
+    {
+#if ENABLE_DATA_LOG
+        closeFile();
+#endif
+        state &= ~STATE_OBD_READY;
+#if VERBOSE
+        SerialInfo.print("Retry");
+#endif
+        byte n = 0;
+        bool toReset = false;
+        while (!init()) {
+#if VERBOSE
+            SerialInfo.write('.');
+#endif
+            Narcoleptic.delay(3000);
+            if (n >= 20) {
+              toReset = true;
+            } else {
+              n++; 
+            }
+        }
+        if (toReset) resetFunc();
+    }
+    bool logOBDData(byte pid)
+    {
+        int value;
+        if (!read(pid, value)) {
+            // error occurred
+            recover();
+            errors++;
+            return false;
+        }
+        dataTime = millis();
+        logData((uint16_t)pid | 0x100, value);
+        errors = 0;
+        return true;
+    }
+    void dataIdleLoop()
+    {
+      if (m_state != OBD_CONNECTED) return;
+#if ENABLE_DATA_LOG
+      flushData();
+#endif
+#if USE_MPU6050 || USE_MPU9150
+      logMEMSData();
+#endif
+    }
+    byte state;
+};
+
+static CLogger logger;
+
+void setup()
+{
+#if VERBOSE
+    SerialInfo.begin(STREAM_BAUDRATE);
+    delay(500);
+    SerialInfo.println("Freematics");
+#endif
+    delay(500);
+    logger.begin();
+    logger.initSender();
+    logger.setup();
+}
+
+void loop()
+{
+    if (logger.state & STATE_OBD_READY) {
+        static byte index2 = 0;
+        for (byte n = 0; n < TIER_NUM1; n++) {
+          byte pid = pgm_read_byte(pidTier1 + n);
+          logger.logOBDData(pid);
+        }
+        byte pid = pgm_read_byte(pidTier2 + index2);
+        logger.logOBDData(pid);
+        index2 = (index2 + 1) % TIER_NUM2;
+        if (logger.errors >= 10) {
+            logger.reconnect();
+        }
+    } else if (!OBD_ATTEMPT_TIME || millis() < OBD_ATTEMPT_TIME * 1000) {
+        if (logger.init()) {
+            logger.state |= STATE_OBD_READY;
+        }
+    }
+
+#if USE_GPS
+    if (logger.state & STATE_GPS_FOUND) {
+        logger.logGPSData();
+    }
+#endif
+}
