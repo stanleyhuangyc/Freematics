9d60d65291e3ceffee6bdd846c371e132c556a3c
Stanley Huang <stanleyhuangyc@gmail.com>
2016-02-26 23:52:29 +1100

    Using multiple PIDs access API

--- firmware_v4/datalogger/datalogger.ino
+++ firmware_v4/datalogger/datalogger.ino
@@ -44,82 +44,61 @@ static uint16_t fileIndex = 0;
 uint16_t MMDD = 0;
 uint32_t UTC = 0;
 
-static const byte PROGMEM pidTier1[]= {PID_RPM, PID_SPEED, PID_ENGINE_LOAD, PID_THROTTLE};
-static const byte PROGMEM pidTier2[] = {PID_COOLANT_TEMP, PID_INTAKE_TEMP, PID_DISTANCE};
-
-#define TIER_NUM1 sizeof(pidTier1)
-#define TIER_NUM2 sizeof(pidTier2)
-
 #if USE_MPU6050
 MEMS_DATA mems;
 #endif
-
-static const byte PROGMEM parts[][3] = {
-  {'S','D'},
-  {'A','C','C'},
-  {'O','B','D'},
-  {'G','P','S'},
-};
-
-typedef enum {
-  PART_SD = 0,
-  PART_MEMS,
-  PART_OBD,
-  PART_GPS,
-} PART_ID;
 
 class ONE : public COBDSPI, public CDataLogger
 {
 public:
     ONE():state(0) {}
-    void showStatus(byte partID, bool OK)
-    {
-#if ENABLE_DATA_OUT
-      char buf[4];
-      memcpy_P(buf, parts[partID], 3);
-      buf[3] = 0;
-      SerialRF.print(buf);
-      SerialRF.print(' ');
-      SerialRF.println(OK ? "OK" : "NO");
-#endif
-    }
     void setup()
     {
-        bool success;
         state = 0;
         
         begin();
+        SerialRF.print("Firmware Version ");
+        SerialRF.println(version);
 
 #if ENABLE_DATA_LOG
+        SerialRF.print("SD ");
         uint16_t volsize = initSD();
         if (volsize) {
-          SerialRF.print("SD ");
           SerialRF.print(volsize);
           SerialRF.println("MB");
         } else {
-          showStatus(PART_SD, false);
+          SerialRF.println("NO");
         }
 #endif
 
 #if USE_MPU6050
+        SerialRF.print("MEMS ");
         Wire.begin();
-        if (success = memsInit()) {
+        if (memsInit()) {
           state |= STATE_MEMS_READY;
+          SerialRF.println("OK");
+        } else {
+          SerialRF.println("NO");
         }
-        showStatus(PART_MEMS, success);
 #endif
 
-        if (success = init()) {
-            state |= STATE_OBD_READY;
+        SerialRF.print("OBD ");
+        if (init()) {
+          state |= STATE_OBD_READY;
+          SerialRF.println("OK");
+        } else {
+          SerialRF.println("NO");
         }
-        showStatus(PART_OBD, success);
 
 #if USE_GPS
-        delay(500);
-        if (success = initGPS(GPS_SERIAL_BAUDRATE)) {
-            state |= STATE_GPS_FOUND;
+        delay(100);
+        SerialRF.print("GPS ");
+        if (initGPS(GPS_SERIAL_BAUDRATE)) {
+          state |= STATE_GPS_FOUND;
+          SerialRF.println("OK");
+        } else {
+          SerialRF.println("NO");
         }
-        showStatus(PART_GPS, success);
 #endif
 
         delay(1000);
@@ -153,12 +132,6 @@ public:
               logData(PID_GPS_ALTITUDE, gd.alt);
               logData(PID_GPS_SPEED, gd.speed);
               logData(PID_GPS_SAT_COUNT, gd.sat);
-              if (UTC == 0) {
-                // first GPS data arrived
-                // to re-create a new date/time named log file
-                closeFile();
-                state &= ~STATE_FILE_READY;
-              }
               // save current date in MMDD format
               unsigned int DDMM = gd.date / 100;
               UTC = gd.time;
@@ -170,11 +143,13 @@ public:
 #endif
     }
 #endif
+#if USE_MPU6050
     void logMEMSData()
     {
         // log the loaded MEMS data
         logData(PID_ACC, mems.value.x_accel / ACC_DATA_RATIO, mems.value.y_accel / ACC_DATA_RATIO, mems.value.z_accel / ACC_DATA_RATIO);
     }
+#endif
 #if ENABLE_DATA_LOG
     uint16_t initSD()
     {
@@ -200,7 +175,7 @@ public:
     }
     void flushData()
     {
-        // flush SD data every 1KB
+      // flush SD data every 1KB
         byte dataSizeKB = dataSize >> 10;
         if (dataSizeKB != lastFileSize) {
 #if VERBOSE
@@ -221,6 +196,7 @@ public:
 #endif
     void reconnect()
     {
+        SerialRF.println("Reconnecting");
         if (init()) {
           // reconnected
           return; 
@@ -242,20 +218,6 @@ public:
         // reset device
         void(* resetFunc) (void) = 0; //declare reset function at address 0
         resetFunc();
-    }
-    bool logOBDData(byte pid)
-    {
-        int value;
-        if (!read(pid, value)) {
-            // error occurred
-            Serial.println("Error");
-            errors++;
-            return false;
-        }
-        dataTime = millis();
-        logData((uint16_t)pid | 0x100, value);
-        errors = 0;
-        return true;
     }
     void dataIdleLoop()
     {
@@ -282,35 +244,59 @@ void setup()
 
 void loop()
 {
+#if ENABLE_DATA_LOG
     if (!(one.state & STATE_FILE_READY) && (one.state & STATE_SD_READY)) {
       if (one.state & STATE_GPS_FOUND) {
         // GPS connected
         if (one.state & STATE_GPS_READY) {
           uint32_t dateTime = (uint32_t)MMDD * 10000 + UTC / 10000;
           if (one.openFile(dateTime) != 0) {
-            UTC = 0;
+            SerialRF.print("FILE ");
+            SerialRF.println(dateTime);
             MMDD = 0;
             one.state |= STATE_FILE_READY;
+          } else {
+            SerialRF.println("File error");
           }
-        } else {
-          delay(3000); 
         }
       } else {
         // no GPS connected 
+        int index = one.openFile(0);
         if (one.openFile(0) != 0) {
           one.state |= STATE_FILE_READY;
+          SerialRF.print("FILE ");
+          SerialRF.println(index);
+        } else {
+            SerialRF.println("File error");
         }
       }
     }
+#endif
     if (one.state & STATE_OBD_READY) {
         static byte index2 = 0;
-        for (byte n = 0; n < TIER_NUM1; n++) {
-          byte pid = pgm_read_byte(pidTier1 + n);
-          one.logOBDData(pid);
+        const byte pids[]= {PID_RPM, PID_SPEED, PID_THROTTLE, PID_ENGINE_LOAD};
+        int values[sizeof(pids)];
+        // read multiple OBD-II PIDs
+        if (one.read(pids, sizeof(pids), values) == sizeof(pids)) {
+          one.dataTime = millis();
+          for (byte n = 0; n < sizeof(pids); n++) {
+            one.logData((uint16_t)pids[n] | 0x100, values[n]);
+          }
         }
-        byte pid = pgm_read_byte(pidTier2 + index2);
-        one.logOBDData(pid);
-        index2 = (index2 + 1) % TIER_NUM2;
+        static byte lastSec = 0;
+        const byte pids2[] = {PID_COOLANT_TEMP, PID_INTAKE_TEMP, PID_AMBIENT_TEMP, PID_DISTANCE};
+        byte sec = (uint8_t)(millis() >> 10);
+        if (sec != lastSec) {
+          // goes in every other second
+          int value;
+          byte pid = pids2[index2 = (index2 + 1) % (sizeof(pids2))];
+          // read single OBD-II PID
+          if (one.isValidPID(pid) && one.read(pid, value)) {
+            one.dataTime = millis();
+            one.logData((uint16_t)pid | 0x100, value);
+            lastSec = sec;
+          }
+        }
         if (one.errors >= 10) {
             one.reconnect();
         }
