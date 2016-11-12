045d543323852c0420b09393776bc50bf2399280
Stanley Huang <stanleyhuangyc@gmail.com>
2016-11-12 20:56:29 +1100

    Several optimizations

--- firmware_v4/telelogger_esp8266/config.h
+++ firmware_v4/telelogger_esp8266/config.h
@@ -12,19 +12,19 @@
 #define STREAM_FORMAT FORMAT_TEXT
 
 #define ENABLE_DATA_CACHE 1
-#define CACHE_SIZE 384 /* bytes */
+#define CACHE_SIZE 400 /* bytes */
 
 // change this to your own
-#define WIFI_SSID "YOUR_SSID"
-#define WIFI_PASSWORD "PASSWORD"
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASSWORD "WIFI_PASS"
 #define SERVER_URL "live.freematics.com.au"
 #define SERVER_PORT 80
 
 /**************************************
 * Accelerometer & Gyro
 **************************************/
-#define USE_MPU6050 1
-#define USE_MPU9250 0
+#define USE_MPU6050 0
+#define USE_MPU9250 1
 #define ACC_DATA_RATIO 172
 #define GYRO_DATA_RATIO 256
 #define COMPASS_DATA_RATIO 8
@@ -42,7 +42,8 @@
 #define MAX_ERRORS_RECONNECT 3
 #define MAX_ERRORS_RESET 6
 #define MAX_CONN_TIME 5000 /* ms */
-#define MAX_HTTP_CONNS 50
+#define MIN_LOOP_TIME 500 /* ms */
+#define MAX_HTTP_CONNS 99
 
 #define SD_CS_PIN 10
 
