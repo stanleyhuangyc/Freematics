ec6eeedb99196fe803b1d98ca3cf36ee1a88e815
Stanley Huang <stanleyhuangyc@gmail.com>
2017-01-22 16:46:23 +1100

    Updatd

--- www/client/freematics.php
+++ www/client/freematics.php
@@ -22,7 +22,9 @@
   curl_close($ch);
 
   // forward response
-  header('Content-Type: application/json');
-  header("Cache-Control: no-cache, must-revalidate");
+  header("Content-Type: application/json");
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
   echo $result;
 ?>
