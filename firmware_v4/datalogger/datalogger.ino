8ee0e21e536abb5d649191b14978addb11359579
Stanley Huang <stanleyhuangyc@gmail.com>
2017-06-30 00:15:01 +1000

    Simplified code

--- firmware_v4/datalogger/datalogger.ino
+++ firmware_v4/datalogger/datalogger.ino
@@ -3,7 +3,7 @@
 * Distributed under BSD license
 * Visit http://freematics.com/products/freematics-one for more information
 * Developed by Stanley Huang <support@freematics.com.au>
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,22 +26,18 @@
 #define STATE_FILE_READY 0x20
 
 static uint8_t lastFileSize = 0;
-static uint16_t fileIndex = 0;
 
 uint16_t MMDD = 0;
 uint32_t UTC = 0;
 
-#if USE_MPU6050 || USE_MPU9250
-byte accCount = 0; // count of accelerometer readings
-long accSum[3] = {0}; // sum of accelerometer data
-int accCal[3] = {0}; // calibrated reference accelerometer data
+#if USE_MPU9250
+int16_t acc[3] = {0};
+int16_t accCal[3] = {0}; // calibrated reference accelerometer data
 byte deviceTemp; // device temperature (celcius degree)
 #endif
 
 class ONE : public COBDSPI, public CDataLogger
-#if USE_MPU6050
-,public CMPU6050
-#elif USE_MPU9250
+#if USE_MPU9250
 ,public CMPU9250
 #endif
 {
@@ -49,7 +45,11 @@ public:
     ONE():state(0) {}
     void setup()
     {
-#if USE_MPU6050 || USE_MPU9250
+      byte ver = begin();
+      Serial.print("Firmware Ver. ");
+      Serial.println(ver);
+
+#if USE_MPU9250
         Serial.print("MEMS ");
         if (memsInit()) {
           state |= STATE_MEMS_READY;
@@ -59,10 +59,7 @@ public:
         }
 #endif
 
-        byte ver = begin();
-        Serial.print("Firmware Ver. ");
-        Serial.println(ver);
-
+#if USE_OBD
         Serial.print("OBD ");
         if (init()) {
           Serial.println("OK");
@@ -71,24 +68,26 @@ public:
           reconnect();
         }
         state |= STATE_OBD_READY;
-
-#if ENABLE_DATA_LOG
-        Serial.print("SD ");
-        uint16_t volsize = initSD();
-        if (volsize) {
-          Serial.print(volsize);
-          Serial.println("MB");
-        } else {
-          Serial.println("NO");
-        }
-#endif
-
-#if 0
         // retrieve VIN
         char buffer[128];
         if ((state & STATE_OBD_READY) && getVIN(buffer, sizeof(buffer))) {
           Serial.print("VIN:");
           Serial.println(buffer);
+        }
+#else
+        SPI.begin();
+#endif
+
+#if ENABLE_DATA_LOG
+        if (!(state & STATE_SD_READY)) {
+          Serial.print("SD ");
+          uint16_t volsize = initSD();
+          if (volsize) {
+            Serial.print(volsize);
+            Serial.println("MB");
+          } else {
+            Serial.println("NO");
+          }
         }
 #endif
 
@@ -102,25 +101,10 @@ public:
           Serial.println("NO");
         }
 #endif
-
-#if USE_MPU6050 || USE_MPU9250
-        calibrateMEMS();
-#endif
     }
 #if USE_GPS
     void logGPSData()
     {
-#if LOG_GPS_NMEA_DATA
-        // issue the command to get NMEA data (one line per request)
-        char buf[255];
-        byte n = gpsGetRawData(buf, sizeof(buf));
-        if (n) {
-            dataTime = millis();
-            // strip heading $GPS and ending \r\n
-            logData(buf + 4, n - 6);
-        }
-#endif
-#if LOG_GPS_PARSED_DATA
         // issue the command to get parsed GPS data
         GPS_DATA gd = {0};
         if (gpsGetData(&gd)) {
@@ -128,14 +112,14 @@ public:
             if (gd.time && gd.time != UTC) {
               byte day = gd.date / 10000;
               if (MMDD % 100 != day) {
-                logData(PID_GPS_DATE, gd.date);
+                log(PID_GPS_DATE, gd.date);
               }
-              logData(PID_GPS_TIME, gd.time);
-              logData(PID_GPS_LATITUDE, gd.lat);
-              logData(PID_GPS_LONGITUDE, gd.lng);
-              logData(PID_GPS_ALTITUDE, gd.alt);
-              logData(PID_GPS_SPEED, gd.speed);
-              logData(PID_GPS_SAT_COUNT, gd.sat);
+              log(PID_GPS_TIME, gd.time);
+              log(PID_GPS_LATITUDE, gd.lat);
+              log(PID_GPS_LONGITUDE, gd.lng);
+              log(PID_GPS_ALTITUDE, gd.alt);
+              log(PID_GPS_SPEED, gd.speed);
+              log(PID_GPS_SAT_COUNT, gd.sat);
               // save current date in MMDD format
               unsigned int DDMM = gd.date / 100;
               UTC = gd.time;
@@ -144,7 +128,6 @@ public:
               state |= STATE_GPS_READY;
             }
         }
-#endif
     }
     void waitGPS()
     {
@@ -171,23 +154,23 @@ public:
     uint16_t initSD()
     {
         state &= ~STATE_SD_READY;
-        pinMode(SS, OUTPUT);
+        pinMode(SD_CS_PIN, OUTPUT);
         if (SD.begin(SD_CS_PIN)) {
           state |= STATE_SD_READY;
-          return SD.cardSize(); 
+          return SD.cardSize();
         } else {
           return 0;
         }
     }
-    void flushData()
+    void flushData(uint32_t fileSize)
     {
       // flush SD data every 1KB
-        byte dataSizeKB = dataSize >> 10;
+        byte dataSizeKB = fileSize >> 10;
         if (dataSizeKB != lastFileSize) {
             flushFile();
             lastFileSize = dataSizeKB;
-#if MAX_LOG_FILE_SIZE
-            if (dataSize >= 1024L * MAX_LOG_FILE_SIZE) {
+#if MAX_DATA_FILE_SIZE
+            if (fileSize >= 1024L * MAX_DATA_FILE_SIZE) {
               closeFile();
               state &= ~STATE_FILE_READY;
             }
@@ -211,75 +194,42 @@ public:
         Serial.println("Standby");
         // put OBD chips into low power mode
         enterLowPowerMode();
-#if USE_MPU6050 || USE_MPU9250
+#if USE_MPU9250
         for (;;) {
-          accSum[0] = 0;
-          accSum[1] = 0;
-          accSum[2] = 0;
-          for (accCount = 0; accCount < 50; ) {
-            readMEMS();
-            sleep(60);
+          unsigned long accSum[3] = {0};
+          unsigned int count = 0;
+          for (uint32_t t = millis(); millis() - t < 1000; count++) {
+            memsRead(acc, 0, 0, 0);
+            accSum[0] += acc[0] * acc[0];
+            accSum[1] += acc[1] * acc[1];
+            accSum[2] += acc[2] * acc[2];
+            delay(10);
           }
           // calculate relative movement
-          unsigned long motion = 0;
-          for (byte i = 0; i < 3; i++) {
-            long n = accSum[i] / accCount - accCal[i];
-            motion += n * n;
-          }
+          unsigned long motion = (accSum[0] + accSum[1] + accSum[2]) / count / count;
+          Serial.print("M:");
+          Serial.println(motion);
           // check movement
           if (motion > WAKEUP_MOTION_THRESHOLD) {
-            Serial.println(motion);
             break;
           }
-          Serial.print('.');
-          calibrateMEMS();
         }
 #else
         while (!init()) sleep(10);
 #endif
         Serial.println("Wakeup");
         leaveLowPowerMode();
-#ifdef ARDUINO_ARCH_AVR
-        // reset device
-        void(* resetFunc) (void) = 0; //declare reset function at address 0
-        resetFunc();
-#else
         setup();
-#endif
     }
-#if USE_MPU6050 || USE_MPU9250
-    void calibrateMEMS()
-    {
-        // store accelerometer reference data
-        if ((state & STATE_MEMS_READY) && accCount) {
-          accCal[0] = accSum[0] / accCount;
-          accCal[1] = accSum[1] / accCount;
-          accCal[2] = accSum[2] / accCount;
-        }
-    }
-    void readMEMS()
-    {
-        // load accelerometer and temperature data
-        int16_t acc[3] = {0};
-        int16_t temp; // device temperature (in 0.1 celcius degree)
-        memsRead(acc, 0, 0, &temp);
-        if (accCount >= 250) {
-          accSum[0] >>= 1;
-          accSum[1] >>= 1;
-          accSum[2] >>= 1;
-          accCount >>= 1;
-        }
-        accSum[0] += acc[0];
-        accSum[1] += acc[1];
-        accSum[2] += acc[2];
-        accCount++;
-        deviceTemp = temp / 10;
-    }
+#if USE_MPU9250
     void dataIdleLoop()
     {
       // do something while waiting for data on SPI
       if (state & STATE_MEMS_READY) {
-        readMEMS();
+        // load accelerometer and temperature data
+        int16_t temp; // device temperature (in 0.1 celcius degree)
+        memsRead(acc, 0, 0, &temp);
+        deviceTemp = temp / 10;
       }
     }
  #endif
@@ -291,7 +241,11 @@ static ONE one;
 void setup()
 {
     Serial.begin(115200);
+#ifdef ESP32
+    Serial.println("Freematics ONE+");
+#else
     Serial.println("Freematics ONE");
+#endif
     delay(1000);
     one.setup();
 }
@@ -302,29 +256,24 @@ void loop()
     if (!(one.state & STATE_FILE_READY) && (one.state & STATE_SD_READY)) {
       if (one.state & STATE_GPS_FOUND) {
         // GPS connected
-        Serial.print("File ");
         if (one.state & STATE_GPS_READY) {
           uint32_t dateTime = (uint32_t)MMDD * 10000 + UTC / 10000;
-          if (one.openFile(dateTime) != 0) {
-            Serial.println(dateTime);
+          if (one.openFile(dateTime)) {
             MMDD = 0;
             one.state |= STATE_FILE_READY;
-          } else {
-            Serial.println("File error");
           }
         }
       } else {
-        // no GPS connected 
-        int index = one.openFile(0);
-        if (index != 0) {
+        // no GPS connected
+        if (one.openFile(0)) {
           one.state |= STATE_FILE_READY;
-          Serial.println(index);
-        } else {
-          Serial.println("File error");
         }
       }
+      delay(1000);
+      return;
     }
 #endif
+#if USE_OBD
     if (one.state & STATE_OBD_READY) {
         byte pids[]= {0, PID_RPM, PID_SPEED, PID_THROTTLE, PID_ENGINE_LOAD};
         byte pids2[] = {PID_COOLANT_TEMP, PID_INTAKE_TEMP, PID_DISTANCE};
@@ -335,7 +284,7 @@ void loop()
         if (one.readPID(pids, sizeof(pids), values) == sizeof(pids)) {
           one.dataTime = millis();
           for (byte n = 0; n < sizeof(pids); n++) {
-            one.logData((uint16_t)pids[n] | 0x100, values[n]);
+            one.log((uint16_t)pids[n] | 0x100, values[n]);
           }
         }
         if (one.errors >= 10) {
@@ -348,17 +297,19 @@ void loop()
         }
       }
     }
-    
+
     // log battery voltage (from voltmeter), data in 0.01v
     int v = one.getVoltage() * 100;
     one.dataTime = millis();
-    one.logData(PID_BATTERY_VOLTAGE, v);
+    one.log(PID_BATTERY_VOLTAGE, v);
+#else
+    one.dataIdleLoop();
+#endif
 
-#if USE_MPU6050 || USE_MPU9250
-    if ((one.state & STATE_MEMS_READY) && accCount > 0) {
+#if USE_MPU9250
+    if (one.state & STATE_MEMS_READY) {
        // log the loaded MEMS data
-      one.logData(PID_ACC, accSum[0] / accCount / ACC_DATA_RATIO, accSum[1] / accCount / ACC_DATA_RATIO, accSum[2] / accCount / ACC_DATA_RATIO);
-      accCount = 0;
+      one.log(PID_ACC, acc[0], acc[1], acc[2]);
     }
 #endif
 #if USE_GPS
@@ -367,12 +318,17 @@ void loop()
     }
 #endif
 
+#if !ENABLE_DATA_OUT
+    Serial.print(one.dataCount);
+    Serial.print(" samples ");
 #if ENABLE_DATA_LOG
-    uint32_t logsize = sdfile.size();
-    if (logsize > 0) {
-      one.flushData();
+    uint32_t fileSize = sdfile.size();
+    if (fileSize > 0) {
+      one.flushData(fileSize);
       Serial.print(sdfile.size());
-      Serial.println(" bytes");
+      Serial.print(" bytes");
     }
+#endif
+    Serial.println();
 #endif
 }
