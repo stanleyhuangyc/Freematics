ee9344558975d4f048f47cf07e22d236b8e25d21
Stanley Huang <stanleyhuangyc@gmail.com>
2017-06-29 23:21:50 +1000

    Fixed some more compiler warnings

--- libraries/FreematicsONE/FreematicsBase.h
+++ libraries/FreematicsONE/FreematicsBase.h
@@ -151,13 +151,13 @@ public:
 	// start xBee UART communication
 	virtual bool xbBegin(unsigned long baudrate = 115200L) = 0;
 	// read data to xBee UART
-	virtual int xbRead(char* buffer, int bufsize, int timeout = 1000) = 0;
+	virtual int xbRead(char* buffer, int bufsize, unsigned int timeout = 1000) = 0;
 	// send data to xBee UART
 	virtual void xbWrite(const char* cmd) = 0;
   // send data to xBee UART
 	virtual void xbWrite(const char* data, int len) = 0;
 	// receive data from xBee UART (returns 0/1/2)
-	virtual byte xbReceive(char* buffer, int bufsize, int timeout = 1000, const char** expected = 0, byte expectedCount = 0) = 0;
+	virtual byte xbReceive(char* buffer, int bufsize, unsigned int timeout = 1000, const char** expected = 0, byte expectedCount = 0) = 0;
 	// purge xBee UART buffer
 	virtual void xbPurge() = 0;
 	// toggle xBee module power
