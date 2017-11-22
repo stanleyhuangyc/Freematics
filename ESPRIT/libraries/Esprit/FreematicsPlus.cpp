dcc875b76d1477be5a66c7ff79ef8a46ba856b6f
Stanley Huang <stanleyhuangyc@gmail.com>
2017-11-22 11:49:57 +1100

    Updated Freematics Esprit library

---
+++ ESPRIT/libraries/Esprit/FreematicsPlus.cpp
@@ -0,0 +1,330 @@
+/*************************************************************************
+* Arduino library for Freematics ONE+ (ESP32)
+* Distributed under BSD license
+* Visit http://freematics.com for more information
+* (C)2017 Developed by Stanley Huang <support@freematics.com.au>
+*************************************************************************/
+
+#include <Arduino.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifdef ESP32
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "soc/uart_struct.h"
+#include "FreematicsSD.h"
+#include "FreematicsPlus.h"
+#include "FreematicsGPS.h"
+
+static TinyGPS* gps = 0;
+static bool newGPSData = false;
+
+#define BEE_UART_PIN_RXD  (16)
+#define BEE_UART_PIN_TXD  (17)
+#define BEE_UART_NUM UART_NUM_1
+
+#define GPS_UART_PIN_RXD  (32)
+#define GPS_UART_PIN_TXD  (33)
+#define GPS_UART_NUM UART_NUM_2
+
+#define UART_BUF_SIZE (2048)
+
+void gps_decode_task(int timeout)
+{
+    if (!gps) {
+        delay(timeout);
+        return;
+    }
+    uint8_t c;
+    int len = uart_read_bytes(GPS_UART_NUM, &c, 1, timeout / portTICK_RATE_MS);
+    if (len == 1) {
+        if (gps->encode(c)) {
+            newGPSData = true;
+        }
+    }
+}
+
+bool gps_get_data(GPS_DATA* gdata)
+{
+	if (!newGPSData || !gps) {
+		return false;
+	}
+    gps->get_datetime((unsigned long*)&gdata->date, (unsigned long*)&gdata->time, 0);
+    gps->get_position((long*)&gdata->lat, (long*)&gdata->lng, 0);
+    gdata->sat = gps->satellites();
+    gdata->speed = gps->speed() * 1852 / 100000; /* km/h */
+    gdata->alt = gps->altitude();
+    gdata->heading = gps->course() / 100;
+    newGPSData = false;
+    return true;
+}
+
+int gps_write_string(const char* string)
+{
+    if (!gps) return 0;
+    return uart_write_bytes(BEE_UART_NUM, string, strlen(string));
+}
+
+bool gps_decode_start()
+{
+    if (gps) return true;
+
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    //Configure UART parameters
+    uart_param_config(GPS_UART_NUM, &uart_config);
+    //Set UART pins
+    uart_set_pin(GPS_UART_NUM, GPS_UART_PIN_TXD, GPS_UART_PIN_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    //Install UART driver (we don't need an event queue here)
+    //In this example we don't even use a buffer for sending data.
+    uart_driver_install(GPS_UART_NUM, UART_BUF_SIZE, 0, 0, NULL, 0);
+
+    // quick check of input data format
+    uint32_t t = millis();
+    uint8_t match[] = {'$', ',', '\n'};
+    int idx = 0;
+    do {
+        uint8_t c;
+        if (uart_read_bytes(GPS_UART_NUM, &c, 1, 200 / portTICK_RATE_MS) == -1) continue;
+        if (c == match[idx]) idx++;
+    } while (millis() - t < GPS_INIT_TIMEOUT && idx < sizeof(match));
+    if (idx < sizeof(match)) {
+        // no matching pattern
+        uart_driver_delete(GPS_UART_NUM);
+        return false;
+    }
+
+    gps = new TinyGPS;
+    return true;
+}
+
+void bee_start()
+{
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    //Configure UART parameters
+    uart_param_config(BEE_UART_NUM, &uart_config);
+    //Set UART pins
+    uart_set_pin(BEE_UART_NUM, BEE_UART_PIN_TXD, BEE_UART_PIN_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    //Install UART driver (we don't need an event queue here)
+    //In this example we don't even use a buffer for sending data.
+    uart_driver_install(BEE_UART_NUM, UART_BUF_SIZE, 0, 0, NULL, 0);
+}
+
+int bee_write_string(const char* string)
+{
+    return uart_write_bytes(BEE_UART_NUM, string, strlen(string));
+}
+
+int bee_write_data(uint8_t* data, int len)
+{
+    return uart_write_bytes(BEE_UART_NUM, (const char*)data, len);
+}
+
+int bee_read(uint8_t* buffer, size_t bufsize, unsigned int timeout)
+{
+    int recv = 0;
+    uint32_t t = millis();
+    do {
+        uint8_t c;
+        int len = uart_read_bytes(BEE_UART_NUM, &c, 1, 0);
+        gps_decode_task(0);
+        if (len == 1) {
+            if (c >= 0xA && c <= 0x7E) {
+                buffer[recv++] = c;
+            }
+        } else if (recv > 0) {
+            break;
+        }
+    } while (recv < bufsize && millis() - t < timeout);
+    return recv;
+}
+
+void bee_flush()
+{
+    uart_flush(BEE_UART_NUM);
+}
+
+extern "C" uint8_t temprature_sens_read();
+
+// get chip temperature sensor
+uint8_t readChipTemperature()
+{
+  return temprature_sens_read();
+}
+
+bool Task::create(void (*task)(void*), const char* name, int priority)
+{
+    if (xHandle) return false;
+    /* Create the task, storing the handle. */
+    BaseType_t xReturned = xTaskCreate(task, name, 4096, (void*)this, priority, &xHandle);
+    return xReturned == pdPASS;
+}
+
+void Task::destroy()
+{
+  if (xHandle) {
+    vTaskDelete((TaskHandle_t)xHandle);
+    xHandle = 0;
+  }
+}
+
+void Task::sleep(uint32_t ms)
+{
+  vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
+Mutex::Mutex()
+{
+  xSemaphore = xSemaphoreCreateMutex();
+  xSemaphoreGive(xSemaphore);
+}
+
+void Mutex::lock()
+{
+  xSemaphoreTake(xSemaphore, portMAX_DELAY);
+}
+
+void Mutex::unlock()
+{
+  xSemaphoreGive(xSemaphore);
+}
+
+bool CFreematicsESP32::gpsInit(unsigned long baudrate)
+{
+	bool success = false;
+	char buf[128];
+	if (baudrate) {
+		pinMode(PIN_GPS_POWER, OUTPUT);
+		// turn on GPS power
+		digitalWrite(PIN_GPS_POWER, HIGH);
+		if (gps_decode_start()) {
+			// success
+			return true;
+		}
+	} else {
+		success = true;
+	}
+	//turn off GPS power
+	digitalWrite(PIN_GPS_POWER, LOW);
+	return success;
+}
+
+bool CFreematicsESP32::gpsGetData(GPS_DATA* gdata)
+{
+  return gps_get_data(gdata);
+}
+
+void CFreematicsESP32::gpsSendCommand(const char* cmd)
+{
+	gps_write_string(cmd);
+}
+
+bool CFreematicsESP32::xbBegin(unsigned long baudrate)
+{
+#ifdef PIN_XBEE_PWR
+	pinMode(PIN_XBEE_PWR, OUTPUT);
+	digitalWrite(PIN_XBEE_PWR, HIGH);
+#endif
+	bee_start();
+  return true;
+}
+
+void CFreematicsESP32::xbWrite(const char* cmd)
+{
+	bee_write_string(cmd);
+#ifdef XBEE_DEBUG
+	Serial.print("<<<");
+	Serial.print(cmd);
+	Serial.println("<<<");
+#endif
+}
+
+void CFreematicsESP32::xbWrite(const char* data, int len)
+{
+	bee_write_data((uint8_t*)data, len);
+}
+
+int CFreematicsESP32::xbRead(char* buffer, int bufsize, unsigned int timeout)
+{
+	return bee_read((uint8_t*)buffer, bufsize, timeout);
+}
+
+int CFreematicsESP32::xbReceive(char* buffer, int bufsize, unsigned int timeout, const char** expected, byte expectedCount)
+{
+	int bytesRecv = 0;
+	uint32_t t = millis();
+	do {
+		if (bytesRecv >= bufsize - 16) {
+			bytesRecv -= dumpLine(buffer, bytesRecv);
+		}
+		int n = xbRead(buffer + bytesRecv, bufsize - bytesRecv - 1, 100);
+		if (n > 0) {
+#ifdef XBEE_DEBUG
+			Serial.print(">>>");
+			buffer[bytesRecv + n] = 0;
+			Serial.print(buffer + bytesRecv);
+			Serial.println(">>>");
+#endif
+			bytesRecv += n;
+			buffer[bytesRecv] = 0;
+            for (byte i = 0; i < expectedCount; i++) {
+                // match expected string(s)
+                if (expected[i] && strstr(buffer, expected[i])) return i + 1;
+            }
+        }
+	} while (millis() - t < timeout);
+	buffer[bytesRecv] = 0;
+	return 0;
+}
+
+void CFreematicsESP32::xbPurge()
+{
+	bee_flush();
+}
+
+void CFreematicsESP32::xbTogglePower()
+{
+#ifdef PIN_XBEE_PWR
+#ifdef XBEE_DEBUG
+	Serial.println("xBee power pin set to low");
+#endif
+	digitalWrite(PIN_XBEE_PWR, LOW);
+	sleep(2000);
+#ifdef XBEE_DEBUG
+	Serial.println("xBee power pin set to high");
+#endif
+	digitalWrite(PIN_XBEE_PWR, HIGH);
+#endif
+}
+
+void CFreematicsESP32::sleep(unsigned int ms)
+{
+	uint32_t t = millis();
+	for (;;) {
+        uint32_t elapsed = millis() - t;
+        if (elapsed >= ms) break;
+		gps_decode_task(ms - elapsed);
+	}
+}
+
+#endif
---
+++ ESPRIT/libraries/Esprit/FreematicsPlus.h
@@ -0,0 +1,279 @@
+/*************************************************************************
+* Arduino library for Freematics ONE+ (ESP32)
+* Distributed under BSD license
+* Visit http://freematics.com for more information
+* (C)2017 Developed by Stanley Huang <support@freematics.com.au>
+*************************************************************************/
+
+#include <Arduino.h>
+#include "FreematicsBase.h"
+#include "FreematicsSD.h"
+
+#ifdef ESP32
+
+// get chip temperature sensor
+uint8_t readChipTemperature();
+
+class Task
+{
+public:
+  Task():xHandle(0) {}
+	bool create(void (*task)(void*), const char* name, int priority = 0);
+  void destroy();
+  static void sleep(uint32_t ms);
+private:
+	void* xHandle;
+};
+
+class Mutex
+{
+public:
+  Mutex();
+  void lock();
+  void unlock();
+private:
+  void* xSemaphore;
+};
+
+class CFreematicsESP32 : public virtual CFreematics
+{
+public:
+  // initialize GPS (set baudrate to 0 to power off GPS)
+  virtual bool gpsInit(unsigned long baudrate = 115200L);
+  // get parsed GPS data
+  virtual bool gpsGetData(GPS_DATA* gdata);
+  // send command string to GPS
+  virtual void gpsSendCommand(const char* cmd);
+	// start xBee UART communication
+	virtual bool xbBegin(unsigned long baudrate = 115200L);
+	// read data to xBee UART
+	virtual int xbRead(char* buffer, int bufsize, unsigned int timeout = 1000);
+	// send data to xBee UART
+	virtual void xbWrite(const char* cmd);
+  // send data to xBee UART
+	virtual void xbWrite(const char* data, int len);
+	// receive data from xBee UART (returns 0/1/2)
+	virtual int xbReceive(char* buffer, int bufsize, unsigned int timeout = 1000, const char** expected = 0, byte expectedCount = 0);
+	// purge xBee UART buffer
+	virtual void xbPurge();
+	// toggle xBee module power
+	virtual void xbTogglePower();
+    // delay specified number of ms while receiving and processing GPS data (ought to be regularly called)
+    virtual void sleep(unsigned int ms);
+};
+
+#endif
+
+class CStorageNull;
+
+class CStorageNull {
+public:
+    virtual bool init() { return true; }
+    virtual void uninit() {}
+    virtual bool begin() { return true; }
+    virtual void end() {}
+    virtual void log(uint16_t pid, int16_t value)
+    {
+        char buf[16];
+        byte len = sprintf_P(buf, PSTR("%X=%d"), pid, value);
+        dispatch(buf, len);
+    }
+    virtual void log(uint16_t pid, int32_t value)
+    {
+        char buf[20];
+        byte len = sprintf_P(buf, PSTR("%X=%ld"), pid, value);
+        dispatch(buf, len);
+    }
+    virtual void log(uint16_t pid, uint32_t value)
+    {
+        char buf[20];
+        byte len = sprintf_P(buf, PSTR("%X=%lu"), pid, value);
+        dispatch(buf, len);
+    }
+    virtual void log(uint16_t pid, int value1, int value2, int value3)
+    {
+        char buf[24];
+        byte len = sprintf_P(buf, PSTR("%X=%d;%d;%d"), pid, value1, value2, value3);
+        dispatch(buf, len);
+    }
+    virtual void logCoordinate(uint16_t pid, int32_t value)
+    {
+        char buf[24];
+        byte len = sprintf_P(buf, PSTR("%X=%d.%06lu"), pid, (int)(value / 1000000), abs(value) % 1000000);
+        dispatch(buf, len);
+    }
+    virtual void timestamp(uint32_t ts)
+    {
+        m_dataTime = ts;
+        if (m_next) m_next->timestamp(ts);
+    }
+    virtual void setForward(CStorageNull* next) { m_next = next; }
+    virtual void flush() {}
+    virtual void purge() { m_samples = 0; }
+    virtual uint16_t samples() { return m_samples; }
+    virtual void dispatch(const char* buf, byte len)
+    {
+        // output data via serial
+        Serial.write((uint8_t*)buf, len);
+        Serial.write('\n');
+        m_samples++;
+        if (m_next) m_next->dispatch(buf, len);
+    }
+protected:
+    byte checksum(const char* data, int len)
+    {
+        byte sum = 0;
+        for (int i = 0; i < len; i++) sum += data[i];
+        return sum;
+    }
+    virtual void header(uint16_t feedid) {}
+    virtual void tailer() {}
+    uint32_t m_dataTime = 0;
+    uint16_t m_samples = 0;
+    CStorageNull* m_next = 0;
+};
+
+class CStorageRAM: public CStorageNull {
+public:
+    bool init(unsigned int cacheSize)
+    {
+      if (m_cacheSize != cacheSize) {
+        uninit();
+        m_cache = new char[m_cacheSize = cacheSize];
+      }
+      return true;
+    }
+    void uninit()
+    {
+        if (m_cache) {
+            delete m_cache;
+            m_cache = 0;
+            m_cacheSize = 0;
+        }
+    }
+    void purge() { m_cacheBytes = 0; m_samples = 0; }
+    unsigned int length() { return m_cacheBytes; }
+    char* buffer() { return m_cache; }
+    void dispatch(const char* buf, byte len)
+    {
+        // reserve some space for checksum
+        int remain = m_cacheSize - m_cacheBytes - len - 3;
+        if (remain < 0) {
+          // m_cache full
+          return;
+        }
+        if (m_dataTime) {
+          // log timestamp
+          uint32_t ts = m_dataTime;
+          m_dataTime = 0;
+          log(0, ts);
+        }
+        // store data in m_cache
+        memcpy(m_cache + m_cacheBytes, buf, len);
+        m_cacheBytes += len;
+        m_cache[m_cacheBytes++] = ',';
+        m_samples++;
+        if (m_next) m_next->dispatch(buf, len);
+    }
+
+    void header(uint16_t feedid)
+    {
+        m_cacheBytes = sprintf(m_cache, "%X#", (unsigned int)feedid);
+    }
+    void tailer()
+    {
+        if (m_cache[m_cacheBytes - 1] == ',') m_cacheBytes--;
+        m_cacheBytes += sprintf(m_cache + m_cacheBytes, "*%X", (unsigned int)checksum(m_cache, m_cacheBytes));
+    }
+protected:
+    unsigned int m_cacheSize = 0;
+    unsigned int m_cacheBytes = 0;
+    char* m_cache = 0;
+};
+
+#ifdef ESP32
+#define SD_CS_PIN 5
+#else
+#define SD_CS_PIN 10
+#endif
+
+class CStorageSD : public CStorageNull {
+public:
+  CStorageSD() {}
+  bool init()
+  {
+      pinMode(SD_CS_PIN, OUTPUT);
+      if(!SD.begin(SD_CS_PIN)){
+          Serial.println("No SD card");
+          return false;
+      }
+      int cardSize = SD.cardSize();
+      Serial.print("SD card size:");
+      Serial.print(cardSize);
+      Serial.println("MB");
+      return true;
+    }
+    bool begin(uint32_t dateTime = 0)
+    {
+      uint16_t fileIndex;
+      char path[20] = "DATA";
+      if (SD.exists(path)) {
+          if (dateTime) {
+             // using date and time as file name
+             sprintf(path + 4, "/%08lu.CSV", dateTime);
+          } else {
+            // use index number as file name
+            for (fileIndex = 1; fileIndex; fileIndex++) {
+                sprintf(path + 4, "/DAT%05u.CSV", fileIndex);
+                if (!SD.exists(path)) {
+                    break;
+                }
+            }
+            if (fileIndex == 0)
+                return false;
+          }
+      } else {
+          SD.mkdir(path);
+          fileIndex = 1;
+          sprintf(path + 4, "/DAT%05u.CSV", 1);
+      }
+      // O_READ | O_WRITE | O_CREAT = 0x13
+      Serial.print("File:");
+      Serial.println(path);
+      sdfile = SD.open(path, 0x13);
+      if (!sdfile) {
+          Serial.println("File error");
+          return false;
+      }
+      sdfile.print("#FREEMATICS");
+      return true;
+  }
+  void end()
+  {
+      if (sdfile) sdfile.close();
+  }
+  void flush()
+  {
+      if (sdfile) sdfile.flush();
+  }
+  void dispatch(const char* buf, byte len)
+  {
+      if (sdfile) {
+        // output data via serial
+        if (m_dataTime) {
+          sdfile.write('\n');
+          sdfile.print(m_dataTime);
+          sdfile.write(':');
+        }
+        sdfile.write((uint8_t*)buf, len);
+        sdfile.write(' ');
+      }
+  }
+  uint32_t size()
+  {
+    return sdfile.size();
+  }
+private:
+  SDClass SD;
+  File sdfile;
+};
