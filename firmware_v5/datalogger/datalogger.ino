48fb7f19dacae7c1b806e0cf804df2f350841dd0
Stanley Huang <stanleyhuangyc@gmail.com>
2017-12-04 15:30:20 +1100

    Improved

--- firmware_v5/datalogger/datalogger.ino
+++ firmware_v5/datalogger/datalogger.ino
@@ -30,66 +30,102 @@
 uint16_t MMDD = 0;
 uint32_t UTC = 0;
 
+#if MEMS_MODE
+
+#if MEMS_MODE == MEMS_ACC
+MPU9250_ACC mems;
+#elif MEMS_MODE == MEMS_9DOF
+MPU9250_9DOF mems;
+#elif MEMS_MODE == MEMS_DMP
+MPU9250_DMP mems;
+#endif
+
+float accBias[3];
+
+void calibrateMEMS()
+{
+    // MEMS data collected while sleeping
+    accBias[0] = 0;
+    accBias[1] = 0;
+    accBias[2] = 0;
+    int n;
+    for (n = 0; n < 100; n++) {
+      float acc[3] = {0};
+      mems.memsRead(acc);
+      accBias[0] += acc[0];
+      accBias[1] += acc[1];
+      accBias[2] += acc[2];
+      delay(10);
+    }
+    accBias[0] /= n;
+    accBias[1] /= n;
+    accBias[2] /= n;
+    Serial.print("ACC Bias:");
+    Serial.print(accBias[0]);
+    Serial.print('/');
+    Serial.print(accBias[1]);
+    Serial.print('/');
+    Serial.println(accBias[2]);
+}
+#endif
+
 class ONE : public COBDSPI, public CDataLogger
 {
 public:
-    ONE():state(0) {}
     void setup()
     {
 #if USE_OBD
       Serial.print("OBD ");
       if (init()) {
         Serial.println("OK");
+        // retrieve VIN
+        char buffer[128];
+        if (checkState(STATE_OBD_READY) && getVIN(buffer, sizeof(buffer))) {
+          Serial.print("VIN:");
+          Serial.println(buffer);
+        }
       } else {
         Serial.println("NO");
         reconnect();
       }
-      state |= STATE_OBD_READY;
+      setState(STATE_OBD_READY);
 #else
       SPI.begin();
 #endif
 
 #if USE_GPS
-      Serial.print("GPS ");
-      if (gpsInit(GPS_SERIAL_BAUDRATE)) {
-        state |= STATE_GPS_FOUND;
-        Serial.println("OK");
-        //waitGPS();
-      } else {
-        Serial.println("NO");
-      }
-#endif
-
-#if USE_OBD
-      // retrieve VIN
-      char buffer[128];
-      if ((state & STATE_OBD_READY) && getVIN(buffer, sizeof(buffer))) {
-        Serial.print("VIN:");
-        Serial.println(buffer);
-      }
-#endif
-
-#if ENABLE_DATA_LOG
-      if (!(state & STATE_SD_READY)) {
-        Serial.print("SD ");
-        uint16_t volsize = initSD();
-        if (volsize) {
-          Serial.print(volsize);
-          Serial.println("MB");
-          state |= STATE_SD_READY;
+      if (!checkState(STATE_GPS_FOUND)) {
+        Serial.print("GPS ");
+        if (gpsInit(GPS_SERIAL_BAUDRATE)) {
+          setState(STATE_GPS_FOUND);
+          Serial.println("OK");
+          //waitGPS();
         } else {
           Serial.println("NO");
         }
       }
 #endif
 
+#if ENABLE_DATA_LOG
+      if (!checkState(STATE_SD_READY)) {
+        Serial.print("SD ");
+        uint16_t volsize = initSD();
+        if (volsize) {
+          Serial.print(volsize);
+          Serial.println("MB");
+          setState(STATE_SD_READY);
+        } else {
+          Serial.println("NO");
+        }
+      }
+#endif
     }
 #if USE_GPS
     void logGPSData()
     {
         // issue the command to get parsed GPS data
         GPS_DATA gd = {0};
-        if (gpsGetData(&gd)) {
+        if (checkState(STATE_GPS_FOUND) && gpsGetData(&gd)) {
             dataTime = millis();
             if (gd.time && gd.time != UTC) {
               byte day = gd.date / 10000;
@@ -107,7 +143,7 @@ public:
               UTC = gd.time;
               MMDD = (DDMM % 100) * 100 + (DDMM / 100);
               // set GPS ready flag
-              state |= STATE_GPS_READY;
+              setState(STATE_GPS_READY);
             }
         }
     }
@@ -153,7 +189,7 @@ public:
 #if MAX_DATA_FILE_SIZE
             if (fileSize >= 1024L * MAX_DATA_FILE_SIZE) {
               closeFile();
-              state &= ~STATE_FILE_READY;
+              clearState(STATE_FILE_READY);
             }
 #endif
         }
@@ -170,30 +206,60 @@ public:
 #if USE_GPS
         gpsInit(0);
 #endif
-        state &= ~(STATE_OBD_READY | STATE_GPS_READY);
-        Serial.println("Standby");
-        digitalWrite(PIN_LED, LOW);
-        // put OBD chips into low power mode
-        while (!init()) {
-          Serial.print('.');
-          delay(10000);
-        }
-        digitalWrite(PIN_LED, HIGH);
-        Serial.println("Wakeup");
-        setup();
-        digitalWrite(PIN_LED, LOW);
+        clearState(STATE_OBD_READY | STATE_GPS_READY);
+        standby();
     }
-    byte state;
+    void standby()
+    {
+  #if ENABLE_GPS
+        if (checkState(STATE_GPS_READY)) {
+          Serial.print("GPS:");
+          gpsInit(0); // turn off GPS power
+          Serial.println("OFF");
+        }
+  #endif
+        clearState(STATE_OBD_READY | STATE_GPS_READY);
+        Serial.println("Standby");
+  #if MEMS_MODE
+        if (checkState(STATE_MEMS_READY)) {
+          calibrateMEMS();
+          for (;;) {
+            // calculate relative movement
+            float motion = 0;
+            for (byte n = 0; n < 10; n++) {
+              float acc[3];
+              mems.memsRead(acc);
+              for (byte i = 0; i < 3; i++) {
+                float m = (acc[i] - accBias[i]);
+                motion += m * m;
+              }
+#ifdef ESP32
+              esp_sleep_enable_timer_wakeup(100000); //100ms
+              esp_light_sleep_start();
+#else
+              delay(100);
+#endif
+            }
+            Serial.println(motion);
+            // check movement
+            if (motion > WAKEUP_MOTION_THRESHOLD) {
+              break;
+            }
+          }
+        }
+  #else
+        while (!init()) Serial.print('.');
+  #endif
+        Serial.println("Wakeup");
+    }
+    bool checkState(byte flags) { return (m_state & flags) == flags; }
+    void setState(byte flags) { m_state |= flags; }
+    void clearState(byte flags) { m_state &= ~flags; }
+private:
+    byte m_state = 0;
 };
 
 ONE one;
-#if MEMS_MODE == MEMS_ACC
-MPU9250_ACC mems;
-#elif MEMS_MODE == MEMS_9DOF
-MPU9250_9DOF mems;
-#elif MEMS_MODE == MEMS_DMP
-MPU9250_DMP mems;
-#endif
 
 void setup()
 {
@@ -214,8 +280,9 @@ void setup()
 #if MEMS_MODE
     Serial.print("MEMS ");
     if (mems.memsInit(ENABLE_ORIENTATION)) {
-      one.state |= STATE_MEMS_READY;
+      one.setState(STATE_MEMS_READY);
       Serial.println("OK");
+      calibrateMEMS();
     } else {
       Serial.println("NO");
     }
@@ -227,18 +294,26 @@ void setup()
 
 void loop()
 {
+#if USE_OBD
+    if (!one.checkState(STATE_OBD_READY)) {
+      digitalWrite(PIN_LED, HIGH);
+      one.setup();
+      digitalWrite(PIN_LED, LOW);
+      return;
+    }
+#endif
 #if ENABLE_DATA_LOG
-    if (!(one.state & STATE_FILE_READY) && (one.state & STATE_SD_READY)) {
+    if (!one.checkState(STATE_FILE_READY) && one.checkState(STATE_SD_READY)) {
       digitalWrite(PIN_LED, HIGH);
 #if USE_GPS
-      if (one.state & STATE_GPS_FOUND) {
+      if (one.checkState(STATE_GPS_FOUND)) {
         // GPS connected
         one.logGPSData();
-        if (one.state & STATE_GPS_READY) {
+        if (one.checkState(STATE_GPS_READY)) {
           uint32_t dateTime = (uint32_t)MMDD * 10000 + UTC / 10000;
           if (one.openFile(dateTime)) {
             MMDD = 0;
-            one.state |= STATE_FILE_READY;
+            one.setState(STATE_FILE_READY);
           }
         } else {
           Serial.println("Waiting for GPS...");
@@ -249,7 +324,7 @@ void loop()
       {
         // no GPS connected
         if (one.openFile(0)) {
-          one.state |= STATE_FILE_READY;
+          one.setState(STATE_FILE_READY);
         }
       }
       one.sleep(1000);
@@ -258,73 +333,60 @@ void loop()
     }
 #endif
 #if USE_OBD
-    if (one.state & STATE_OBD_READY) {
-        byte pids[]= {PID_RPM, PID_SPEED, PID_THROTTLE, PID_ENGINE_LOAD};
-        one.dataTime = millis();
-        for (byte i = 0; i < sizeof(pids) / sizeof(pids[0]); i++) {
-          int value;
-          byte pid = pids[i];
-          if (one.readPID(pid, value)) {
-            one.log((uint16_t)pids[i] | 0x100, value);
-          }
-          // process MEMS data every time a PID is read to ensure data frequency
-#endif
-#if MEMS_MODE
-          if (one.state & STATE_MEMS_READY) {
-            float acc[3];
-            bool updated;
-#if ENABLE_ORIENTATION
-            ORIENTATION ori;
-#if !ENABLE_DMP
-            float gyr[3];
-            float mag[3];
-            updated = mems.memsRead(acc, gyr, mag, 0, &ori);
-#else
-            updated = mems.memsRead(acc, 0, 0, 0, &ori);
-#endif
-            if (updated) {
-              Serial.print("Orientation: ");
-              Serial.print(ori.yaw, 2);
-              Serial.print(' ');
-              Serial.print(ori.pitch, 2);
-              Serial.print(' ');
-              Serial.println(ori.roll, 2);
-              one.log(PID_ACC, (int16_t)(acc[0] * 100), (int16_t)(acc[1] * 100), (int16_t)(acc[2] * 100));
-              one.log(PID_ORIENTATION, (int16_t)(ori.yaw * 100), (int16_t)(ori.pitch * 100), (int16_t)(ori.roll * 100));
-            }
-#else
-            updated = mems.memsRead(acc);
-            if (updated) {
-              one.log(PID_ACC, (int16_t)(acc[0] * 100), (int16_t)(acc[1] * 100), (int16_t)(acc[2] * 100));
-            }
-#endif
-          }
-#endif
-#if USE_OBD
-        }
-        if (one.errors >= 3) {
-            one.reset();
-            one.reconnect();
-        }
-    } else {
-      if (!OBD_ATTEMPT_TIME || millis() < OBD_ATTEMPT_TIME * 1000) {
-        digitalWrite(PIN_LED, HIGH);
-        if (one.init()) {
-            one.state |= STATE_OBD_READY;
-        }
-        digitalWrite(PIN_LED, LOW);
+    byte pids[]= {PID_RPM, PID_SPEED, PID_THROTTLE, PID_ENGINE_LOAD};
+    one.dataTime = millis();
+    for (byte i = 0; i < sizeof(pids) / sizeof(pids[0]); i++) {
+      int value;
+      byte pid = pids[i];
+      if (one.readPID(pid, value)) {
+        one.log((uint16_t)pids[i] | 0x100, value);
+      } else if (one.errors >= 3) {
+          one.reset();
+          one.reconnect();
       }
-    }
+#endif
 
+#if MEMS_MODE
+      if (one.checkState(STATE_MEMS_READY)) {
+        float acc[3];
+        bool updated;
+#if ENABLE_ORIENTATION
+        ORIENTATION ori;
+#if !ENABLE_DMP
+        float gyr[3];
+        float mag[3];
+        updated = mems.memsRead(acc, gyr, mag, 0, &ori);
+#else
+        updated = mems.memsRead(acc, 0, 0, 0, &ori);
+#endif
+        if (updated) {
+          Serial.print("Orientation: ");
+          Serial.print(ori.yaw, 2);
+          Serial.print(' ');
+          Serial.print(ori.pitch, 2);
+          Serial.print(' ');
+          Serial.println(ori.roll, 2);
+          one.log(PID_ACC, (int16_t)(acc[0] * 100), (int16_t)(acc[1] * 100), (int16_t)(acc[2] * 100));
+          one.log(PID_ORIENTATION, (int16_t)(ori.yaw * 100), (int16_t)(ori.pitch * 100), (int16_t)(ori.roll * 100));
+        }
+#else
+        updated = mems.memsRead(acc);
+        if (updated) {
+          one.log(PID_ACC, (int16_t)(acc[0] * 100), (int16_t)(acc[1] * 100), (int16_t)(acc[2] * 100));
+        }
+#endif
+      }
+#endif
+
+#if USE_OBD
+    }
     // log battery voltage (from voltmeter), data in 0.01v
     int v = one.getVoltage() * 100;
     one.log(PID_BATTERY_VOLTAGE, v);
 #endif
 
 #if USE_GPS
-    if (one.state & STATE_GPS_FOUND) {
-      one.logGPSData();
-    }
+    one.logGPSData();
 #endif
 
 #if !ENABLE_DATA_OUT
