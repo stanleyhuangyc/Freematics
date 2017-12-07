981d0b9a1ac4a7bf44e30f8f90a84c6e538797f5
Stanley Huang <stanleyhuangyc@gmail.com>
2017-12-07 17:30:07 +1100

    Fixed compiliation issue with latest ESP-IDF

--- ESPRIT/libraries/Esprit/utility/gatts.c
+++ ESPRIT/libraries/Esprit/utility/gatts.c
@@ -215,11 +215,10 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
         gatt_connected = 0;
         break;
     case ESP_GATTS_CONNECT_EVT:
-        ESP_LOGI(GATTS_TAG, "SERVICE_START_EVT, conn_id %d, remote %02x:%02x:%02x:%02x:%02x:%02x:, is_conn %d\n",
+        ESP_LOGI(GATTS_TAG, "SERVICE_START_EVT, conn_id %d, remote %02x:%02x:%02x:%02x:%02x:%02x\n",
                  param->connect.conn_id,
                  param->connect.remote_bda[0], param->connect.remote_bda[1], param->connect.remote_bda[2],
-                 param->connect.remote_bda[3], param->connect.remote_bda[4], param->connect.remote_bda[5],
-                 param->connect.is_connected);
+                 param->connect.remote_bda[3], param->connect.remote_bda[4], param->connect.remote_bda[5]);
         gl_profile_tab[PROFILE_A_APP_ID].conn_id = param->connect.conn_id;
         gatt_connected = 1;
         break;
