6b629261a49a50a9fefcb4899e3ffb1b232a05ea
Stanley Huang <stanleyhuangyc@gmail.com>
2018-02-07 20:49:50 +1100

    Update config

--- firmware_v5/datalogger/config.h
+++ firmware_v5/datalogger/config.h
@@ -6,6 +6,10 @@
 #define MEMS_9DOF 2
 #define MEMS_DMP 3
 
+#define STORAGE_NONE 0
+#define STORAGE_SD 1
+#define STORAGE_SPIFFS 2
+
 /**************************************
 * Data logging/streaming out
 **************************************/
@@ -16,7 +20,7 @@
 #define ENABLE_DATA_OUT 0
 
 // file size limit
-#define MAX_DATA_FILE_SIZE 1024 /* KB */
+#define MAX_DATA_FILE_SIZE 1024000 /* KB */
 
 /**************************************
 * Hardware setup
@@ -30,14 +34,19 @@
 
 #endif
 
-// enable(1)/disable(0) MEMS
-#ifndef MEMS_MODE
-#define MEMS_MODE MEMS_ACC
-#endif
-
 // enable(1)/disable(0) quaternion calculation to get orientation
 #ifndef ENABLE_ORIENTATION
 #define ENABLE_ORIENTATION 0
+#endif
+
+#if ENABLE_ORIENTATION
+#define MEMS_MODE MEMS_9DOF
+#elif !defined(MEMS_MODE)
+#define MEMS_MODE MEMS_ACC
+#endif
+
+#ifndef STORAGE
+#define STORAGE STORAGE_SD
 #endif
 
 // GPS parameters
