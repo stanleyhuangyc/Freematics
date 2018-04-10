90638a5103a96452cb9bc6555362d7b3c48b819d
Stanley Huang <stanleyhuangyc@gmail.com>
2018-04-10 22:43:35 +1000

    Improve DMP code for MPU9250

--- libraries/FreematicsPlus/utility/arduino_mpu9250_i2c.h
+++ libraries/FreematicsPlus/utility/arduino_mpu9250_i2c.h
@@ -8,12 +8,6 @@ This library implements motion processing functions of Invensense's MPU-9250.
 It is based on their Emedded MotionDriver 6.12 library.
 	https://www.invensense.com/developers/software-downloads/
 
-Development environment specifics:
-Arduino IDE 1.6.12
-SparkFun 9DoF Razor IMU M0
-
-Supported Platforms:
-- ATSAMD21 (Arduino Zero, SparkFun SAMD21 Breakouts)
 ******************************************************************************/
 #ifndef _ARDUINO_MPU9250_I2C_H_
 #define _ARDUINO_MPU9250_I2C_H_
