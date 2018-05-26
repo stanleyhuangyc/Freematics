41c7a0ba902b377d045af430f19171317bbde367
Stanley Huang <stanleyhuangyc@gmail.com>
2018-05-26 21:39:02 +1000

    ESP32 GPS logger

---
+++ ESPRIT/gps_logger/dataserver.cpp
@@ -0,0 +1,360 @@
+/*************************************************************************
+* Vehicle Telemetry Data Logger for Freematics ONE+
+*
+* Developed by Stanley Huang <stanley@freematics.com.au>
+* Distributed under BSD license
+* Visit https://freematics.com/products/freematics-one-plus for more info
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+* Implemented HTTP APIs:
+* /api/info - device info
+* /api/live - live data (OBD/GPS/MEMS)
+* /api/control - issue a control command
+* /api/list - list of log files
+* /api/log/<file #> - raw CSV format log file
+* /api/data/<file #>?pid=<PID in hex> - JSON array of PID data
+*************************************************************************/
+
+#include <FreematicsPlus.h>
+#if defined(ESP32)
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <SPIFFS.h>
+#include <apps/sntp/sntp.h>
+#include <esp_spi_flash.h>
+#include <esp_err.h>
+#else
+#error Unsupported board type
+#endif
+#include <httpd.h>
+#include "config.h"
+
+#define WIFI_TIMEOUT 5000
+
+extern uint32_t fileid;
+
+extern "C"
+{
+uint8_t temprature_sens_read();
+uint32_t hall_sens_read();
+}
+
+extern SDClass SD;
+
+HttpParam httpParam;
+
+int handlerLiveData(UrlHandlerParam* param);
+int handlerControl(UrlHandlerParam* param);
+
+uint16_t hex2uint16(const char *p);
+
+int handlerInfo(UrlHandlerParam* param)
+{
+    char *buf = param->pucBuffer;
+    int bufsize = param->bufSize;
+    int bytes = snprintf(buf, bufsize, "{\"httpd\":{\"uptime\":%lu,\"clients\":%d,\"requests\":%u,\"traffic\":%u},\n",
+        millis(), httpParam.stats.clientCount, (unsigned int)httpParam.stats.reqCount, (unsigned int)(httpParam.stats.totalSentBytes >> 10));
+
+    time_t now;
+    time(&now);
+    struct tm timeinfo = { 0 };
+    localtime_r(&now, &timeinfo);
+    if (timeinfo.tm_year) {
+        bytes += snprintf(buf + bytes, bufsize - bytes, "\"rtc\":{\"date\":\"%04u-%02u-%02u\",\"time\":\"%02u:%02u:%02u\"},\n",
+        timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday,
+        timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+    }
+
+    int deviceTemp = (int)temprature_sens_read() * 165 / 255 - 40;
+    bytes += snprintf(buf + bytes, bufsize - bytes, "\"cpu\":{\"temperature\":%d,\"magnetic\":%d},\n",
+        deviceTemp, hall_sens_read());
+
+#if STORAGE == STORAGE_SPIFFS
+    bytes += snprintf(buf + bytes, bufsize - bytes, "\"spiffs\":{\"total\":%u,\"used\":%u}",
+        SPIFFS.totalBytes(), SPIFFS.usedBytes());
+#else
+    bytes += snprintf(buf + bytes, bufsize - bytes, "\"sd\":{\"total\":%u}", SD.cardSize());
+#endif
+
+    if (bytes < bufsize - 1) buf[bytes++] = '}';
+
+    param->contentLength = bytes;
+    param->fileType=HTTPFILETYPE_JSON;
+    return FLAG_DATA_RAW;
+}
+
+class LogDataContext {
+public:
+#if STORAGE == STORAGE_SPIFFS
+    fs::File file;
+#else
+    SDLib::File file;
+#endif
+    uint32_t tsStart;
+    uint32_t tsEnd;
+    uint16_t pid;
+};
+
+int handlerLogFile(UrlHandlerParam* param)
+{
+    uint32_t duration = 0;
+    LogDataContext* ctx = (LogDataContext*)param->hs->ptr;
+    param->fileType = HTTPFILETYPE_TEXT;
+    if (ctx) {
+		if (!param->pucBuffer) {
+			// connection to be closed, final calling, cleanup
+			ctx->file.close();
+            delete ctx;
+			param->hs->ptr = 0;
+			return 0;
+		}
+    } else {
+        int id = 0;
+        if (param->pucRequest[0] == '/') {
+            id = atoi(param->pucRequest + 1);
+        }
+        sprintf(param->pucBuffer, "/DATA/%u.CSV", id == 0 ? fileid : id);
+        ctx = new LogDataContext;
+#if STORAGE == STORAGE_SPIFFS
+        ctx->file = SPIFFS.open(param->pucBuffer, FILE_READ);
+#else
+        ctx->file = SD.open(param->pucBuffer, SD_FILE_READ);
+#endif
+        if (!ctx->file) {
+            strcat(param->pucBuffer, " not found");
+            param->contentLength = strlen(param->pucBuffer);
+            delete ctx;
+            return FLAG_DATA_RAW;
+        }
+        param->hs->ptr = (void*)ctx;
+    }
+
+    if (!ctx->file.available()) {
+        // EOF
+        return 0;
+    }
+    param->contentLength = ctx->file.readBytes(param->pucBuffer, param->bufSize);
+    param->fileType = HTTPFILETYPE_TEXT;
+    return FLAG_DATA_STREAM;
+}
+
+int handlerLogData(UrlHandlerParam* param)
+{
+    uint32_t duration = 0;
+    LogDataContext* ctx = (LogDataContext*)param->hs->ptr;
+    param->fileType = HTTPFILETYPE_JSON;
+    if (ctx) {
+		if (!param->pucBuffer) {
+			// connection to be closed, final calling, cleanup
+			ctx->file.close();
+            delete ctx;
+			param->hs->ptr = 0;
+			return 0;
+		}
+    } else {
+        int id = 0;
+        if (param->pucRequest[0] == '/') {
+            id = atoi(param->pucRequest + 1);
+        }
+        sprintf(param->pucBuffer, "/DATA/%u.CSV", id == 0 ? fileid : id);
+        ctx = new LogDataContext;
+#if STORAGE == STORAGE_SPIFFS
+        ctx->file = SPIFFS.open(param->pucBuffer, FILE_READ);
+#else
+        ctx->file = SD.open(param->pucBuffer, SD_FILE_READ);
+#endif
+        if (!ctx->file) {
+            param->contentLength = sprintf(param->pucBuffer, "{\"error\":\"Data file not found\"}");
+            delete ctx;
+            return FLAG_DATA_RAW;
+        }
+        ctx->pid = mwGetVarValueHex(param->pxVars, "pid", 0);
+        ctx->tsStart = mwGetVarValueInt(param->pxVars, "start", 0);
+        ctx->tsEnd = 0xffffffff;
+        duration = mwGetVarValueInt(param->pxVars, "duration", 0);
+        if (ctx->tsStart && duration) {
+            ctx->tsEnd = ctx->tsStart + duration;
+            duration = 0;
+        }
+        param->hs->ptr = (void*)ctx;
+        // JSON head
+        param->contentLength = sprintf(param->pucBuffer, "[");
+    }
+    
+    int len = 0;
+    char buf[64];
+    uint32_t ts = 0;
+
+    for (;;) {
+        int c = ctx->file.read();
+        if (c == -1) {
+            if (param->contentLength == 0) {
+                // EOF
+                return 0;
+            }
+            // JSON tail
+            if (param->pucBuffer[param->contentLength - 1] == ',') param->contentLength--;
+            param->pucBuffer[param->contentLength++] = ']';
+            break;
+        }
+        if (c == '\n') {
+            // line end, process the line
+            buf[len] = 0;
+            char *value = strchr(buf, ',');
+            if (value++) {
+                uint16_t pid = hex2uint16(buf);
+                if (pid == 0) {
+                    // timestamp
+                    ts = atoi(value);
+                    if (duration) {
+                        ctx->tsEnd = ts + duration;
+                        duration = 0;
+                    }
+                } else if (pid == ctx->pid && ts >= ctx->tsStart && ts < ctx->tsEnd) {
+                    // generate json array element
+                    param->contentLength += snprintf(param->pucBuffer + param->contentLength, param->bufSize - param->contentLength,
+                        "[%u,%s],", ts, value);
+                }
+            }
+            len = 0;
+            if (param->contentLength + 32 > param->bufSize) break;
+        } else if (len < sizeof(buf) - 1) {
+            buf[len++] = c;
+        }
+    }
+    return FLAG_DATA_STREAM;
+}
+
+int handlerLogList(UrlHandlerParam* param)
+{
+    char *buf = param->pucBuffer;
+    int bufsize = param->bufSize;
+#if STORAGE == STORAGE_SPIFFS
+    fs::File root = SPIFFS.open("/");
+    fs::File file;
+#elif STORAGE == STORAGE_SD
+    SDLib::File root = SD.open("/DATA");
+    SDLib::File file;
+#endif
+    int n = snprintf(buf, bufsize, "[");
+    if (root) {
+        while(file = root.openNextFile()) {
+            const char *fn = file.name();
+#if STORAGE == STORAGE_SPIFFS
+            if (!strncmp(fn, "/DATA/", 6)) {
+                fn += 6;
+#else
+            if (1) {
+#endif
+                unsigned int size = file.size();
+                Serial.print(fn);
+                Serial.print(' ');
+                Serial.print(size);
+                Serial.println(" bytes");
+                unsigned int id = atoi(fn);
+                if (id) {
+                    n += snprintf(buf + n, bufsize - n, "{\"id\":%u,\"size\":%u",
+                        id, size);
+                    if (id == fileid) {
+                        n += snprintf(buf + n, bufsize - n, ",\"active\":true");
+                    }
+                    n += snprintf(buf + n, bufsize - n, "},");
+                }
+            }
+        }
+        if (buf[n - 1] == ',') n--;
+    }
+    n += snprintf(buf + n, bufsize - n, "]");
+    param->fileType=HTTPFILETYPE_JSON;
+    param->contentLength = n;
+    return FLAG_DATA_RAW;
+}
+
+UrlHandler urlHandlerList[]={
+    {"api/live", handlerLiveData},
+    {"api/info", handlerInfo},
+    {"api/control", handlerControl},
+#if STORAGE != STORAGE_NONE
+    {"api/list", handlerLogList},
+    {"api/data", handlerLogData},
+    {"api/log", handlerLogFile},
+#endif
+    {0}
+};
+
+void obtainTime()
+{
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, (char*)"pool.ntp.org");
+    sntp_init();
+}
+
+void serverProcess(int timeout)
+{
+    mwHttpLoop(&httpParam, timeout);
+}
+
+void serverCheckup()
+{
+#if ENABLE_WIFI_STATION
+    static uint32_t wifiStartTime = 0;
+    if (WiFi.status() != WL_CONNECTED) {
+        if (wifiStartTime == 0 || millis() - wifiStartTime > WIFI_JOIN_TIMEOUT) {
+            WiFi.disconnect(false);
+            Serial.print("Connecting to hotspot (SSID:");
+            Serial.print(WIFI_SSID);
+            Serial.println(')');
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+            wifiStartTime = millis();
+        }
+    } else {
+        if (wifiStartTime) {
+            // just connected
+            Serial.print("Connected to hotspot. IP:");
+            Serial.println(WiFi.localIP());
+
+            // start mDNS responder
+            MDNS.begin("datalogger");
+            MDNS.addService("http", "tcp", 80);
+
+            wifiStartTime = 0;
+        }
+
+    }
+#endif
+}
+
+bool serverSetup()
+{
+#if ENABLE_WIFI_AP && ENABLE_WIFI_STATION
+    WiFi.mode (WIFI_AP_STA);
+#elif ENABLE_WIFI_AP
+    WiFi.mode (WIFI_AP);
+#elif ENABLE_WIFI_STATION
+    WiFi.mode (WIFI_STA);
+#endif
+
+#if ENABLE_WIFI_AP
+    WiFi.softAP(WIFI_AP_SSID, WIFI_AP_PASSWORD);
+    Serial.print("AP:");
+    Serial.print(WiFi.softAPIP());
+    Serial.print(' ');
+#endif
+
+    mwInitParam(&httpParam, 80, "/spiffs");
+    httpParam.pxUrlHandler = urlHandlerList;
+
+    if (mwServerStart(&httpParam)) {
+        return false;
+    }
+
+    obtainTime();
+    return true;
+}
---
+++ ESPRIT/gps_logger/gps_logger.ino
@@ -0,0 +1,360 @@
+/*************************************************************************
+* Vehicle Telemetry Data Logger for Freematics ONE+
+*
+* Developed by Stanley Huang <stanley@freematics.com.au>
+* Distributed under BSD license
+* Visit https://freematics.com/products/freematics-one-plus for more info
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*************************************************************************/
+
+#include <FreematicsPlus.h>
+#include <httpd.h>
+#include "datalogger.h"
+#include "config.h"
+
+// states
+#define STATE_STORE_READY 0x1
+#define STATE_GPS_FOUND 0x4
+#define STATE_GPS_READY 0x8
+#define STATE_FILE_READY 0x20
+
+void serverProcess(int timeout);
+bool serverSetup();
+void serverCheckup();
+void executeCommand();
+
+#ifdef ESP32
+extern "C"
+{
+uint8_t temprature_sens_read();
+uint32_t hall_sens_read();
+}
+#endif
+
+uint16_t MMDD = 0;
+uint32_t UTC = 0;
+uint32_t startTime = 0;
+uint32_t pidErrors = 0;
+uint32_t fileid = 0;
+
+GPS_DATA gd = {0};
+
+char command[16] = {0};
+
+GATTServer ble;
+FreematicsESP32 sys;
+
+class DataOutputter : public NullLogger
+{
+    void write(const char* buf, byte len)
+    {
+#if ENABLE_SERIAL_OUT
+        Serial.println(buf);
+        ble.println(buf);
+#endif
+    }
+};
+
+#if STORAGE == STORAGE_SD
+SDLogger store(new DataOutputter);
+#elif STORAGE == STORAGE_SPIFFS
+SPIFFSLogger store(new DataOutputter);
+#else
+DataOutputter store;
+#endif
+
+int handlerLiveData(UrlHandlerParam* param)
+{
+    char *buf = param->pucBuffer;
+    int bufsize = param->bufSize;
+    int n = 0;
+    n += snprintf(buf + n, bufsize - n, "{\"gps\":{\"lat\":%d,\"lng\":%d,\"alt\":%d,\"speed\":%d,\"sat\":%d}",
+        gd.lat, gd.lng, gd.alt, gd.speed, gd.sat);
+    buf[n++] = '}';
+    param->contentLength = n;
+    param->fileType=HTTPFILETYPE_JSON;
+    return FLAG_DATA_RAW;
+}
+
+int handlerControl(UrlHandlerParam* param)
+{
+    char *cmd = mwGetVarValue(param->pxVars, "cmd", 0);
+    if (!cmd) return 0;
+    char *buf = param->pucBuffer;
+    int bufsize = param->bufSize;
+    if (command[0]) {
+        param->contentLength = snprintf(buf, bufsize, "{\"result\":\"pending\"}");
+    } else {
+        strncpy(command, cmd, sizeof(command) - 1);
+        param->contentLength = snprintf(buf, bufsize, "{\"result\":\"OK\"}");
+    }
+    param->fileType=HTTPFILETYPE_JSON;
+    return FLAG_DATA_RAW;
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels)
+{
+    Serial.printf("Listing directory: %s\n", dirname);
+    fs::File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    fs::File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print(file.name());
+            Serial.print(' ');
+            Serial.print(file.size());
+            Serial.println(" bytes");
+        }
+        file = root.openNextFile();
+    }
+}
+
+class DataLogger
+{
+public:
+    void init()
+    {
+      if (!checkState(STATE_GPS_FOUND)) {
+        Serial.print("GPS...");
+        if (sys.gpsInit(GPS_SERIAL_BAUDRATE)) {
+          setState(STATE_GPS_FOUND);
+          Serial.println("OK");
+          //waitGPS();
+        } else {
+          Serial.println("NO");
+        }
+      }
+
+      startTime = millis();
+    }
+    void logGPSData()
+    {
+        // issue the command to get parsed GPS data
+        if (checkState(STATE_GPS_FOUND) && sys.gpsGetData(&gd)) {
+            store.setTimestamp(millis());
+            if (gd.time && gd.time != UTC) {
+              byte day = gd.date / 10000;
+              if (MMDD % 100 != day) {
+                store.log(PID_GPS_DATE, gd.date);
+              }
+              store.log(PID_GPS_TIME, gd.time);
+              store.log(PID_GPS_LATITUDE, gd.lat);
+              store.log(PID_GPS_LONGITUDE, gd.lng);
+              store.log(PID_GPS_ALTITUDE, gd.alt);
+              store.log(PID_GPS_SPEED, gd.speed);
+              store.log(PID_GPS_SAT_COUNT, gd.sat);
+              // save current date in MMDD format
+              unsigned int DDMM = gd.date / 100;
+              UTC = gd.time;
+              MMDD = (DDMM % 100) * 100 + (DDMM / 100);
+              // set GPS ready flag
+              setState(STATE_GPS_READY);
+            }
+        }
+    }
+    void logSensorData()
+    {
+        int deviceTemp = (int)temprature_sens_read() * 165 / 255 - 40;
+        int hall = hall_sens_read();
+        store.log(PID_DEVICE_TEMP, deviceTemp);
+        store.log(PID_DEVICE_HALL, hall);
+    }
+    void waitGPS()
+    {
+        int elapsed = 0;
+        GPS_DATA gd = {0};
+        for (uint32_t t = millis(); millis() - t < 300000;) {
+          int t1 = (millis() - t) / 1000;
+          if (t1 != elapsed) {
+            Serial.print("Waiting for GPS (");
+            Serial.print(elapsed);
+            Serial.println(")");
+            elapsed = t1;
+          }
+          // read parsed GPS data
+          if (sys.gpsGetData(&gd) && gd.sat != 0 && gd.sat != 255) {
+            Serial.print("SAT:");
+            Serial.println(gd.sat);
+            break;
+          }
+        }
+    }
+    void checkFileSize(uint32_t fileSize)
+    {
+        static uint8_t lastSizeKB = 0;
+        static uint8_t flushCount = 0;
+        uint8_t sizeKB = fileSize >> 10;
+        if (sizeKB != lastSizeKB) {
+            lastSizeKB = sizeKB;
+            flushCount = 0;
+        } else if (++flushCount == 100) {
+          // if file size does not increase after many flushes, close file
+          store.close();
+          clearState(STATE_FILE_READY);
+          flushCount = 0;
+        }
+    }
+    bool checkState(byte flags) { return (m_state & flags) == flags; }
+    void setState(byte flags) { m_state |= flags; }
+    void clearState(byte flags) { m_state &= ~flags; }
+private:
+    byte m_state = 0;
+};
+
+DataLogger logger;
+
+void showStats()
+{
+    uint32_t t = millis() - startTime;
+    uint32_t dataCount = store.getDataCount();
+    // calculate samples per second
+    float sps = (float)dataCount * 1000 / t;
+    // output to serial monitor
+    char timestr[24];
+    sprintf(timestr, "%02u:%02u.%c", t / 60000, (t % 60000) / 1000, (t % 1000) / 100 + '0');
+    uint32_t fileSize = store.size();
+    Serial.print(timestr);
+    Serial.print(" | ");
+    Serial.print(dataCount);
+    Serial.print(" samples | ");
+    Serial.print(sps, 1);
+    Serial.print(" sps");
+    if (fileSize > 0) {
+      logger.checkFileSize(fileSize);
+      Serial.print(" | ");
+      Serial.print(fileSize);
+      Serial.print(" bytes");
+    }
+    Serial.println();
+    // output via BLE
+    ble.print(timestr);
+    ble.print(' ');
+    ble.print(dataCount);
+    ble.print(' ');
+    ble.print(sps, 1);
+    if (fileSize > 0) {
+      ble.print(' ');
+      ble.print(fileSize >> 10);
+      ble.print('K');
+    }
+    ble.println();
+}
+
+void setup()
+{
+    delay(1000);
+
+    // initialize USB serial
+    Serial.begin(115200);
+    Serial.print("ESP32 ");
+    Serial.print(ESP.getCpuFreqMHz());
+    Serial.print("MHz ");
+    Serial.print(getFlashSize() >> 10);
+    Serial.println("MB Flash");
+
+    sys.begin();
+    ble.begin(WIFI_AP_SSID);
+
+    // init LED pin
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_LED, HIGH);
+
+#if STORAGE == STORAGE_SD
+    Serial.print("SD...");
+    int volsize = store.begin();
+    if (volsize > 0) {
+      Serial.print(volsize);
+      Serial.println("MB");
+      logger.setState(STATE_STORE_READY);
+    } else {
+      Serial.println("NO");
+    }
+#elif STORAGE == STORAGE_SPIFFS
+    Serial.print("SPIFFS...");
+    int freebytes = store.begin();
+    if (freebytes >= 0) {
+      Serial.print(freebytes >> 10);
+      Serial.println(" KB free");
+      logger.setState(STATE_STORE_READY);
+      listDir(SPIFFS, "/", 0);
+    } else {
+      Serial.println("NO");
+    }
+#endif
+
+#if ENABLE_HTTPD
+    Serial.print("HTTP Server...");
+    if (serverSetup()) {
+      Serial.println("OK");
+    } else {
+      Serial.println("NO");
+    }
+    serverCheckup();
+#endif
+
+    pinMode(PIN_LED, LOW);
+
+    logger.init();
+}
+
+void executeCommand()
+{
+    if (!command[0]) return;
+    if (!strcmp(command, "reset")) {
+        store.close();
+        ESP.restart();
+        // never reach here
+    }
+    command[0] = 0;
+}
+
+void loop()
+{
+    // if file not opened, create a new file
+    if (logger.checkState(STATE_STORE_READY) && !logger.checkState(STATE_FILE_READY)) {
+      fileid = store.open();
+      if (fileid) {
+        logger.setState(STATE_FILE_READY);
+      }
+    }
+
+    uint32_t ts = millis();
+    
+    store.setTimestamp(ts);
+    logger.logGPSData();
+    logger.logSensorData();
+
+#if ENABLE_HTTPD
+    serverProcess(10);
+    serverCheckup();
+#endif
+
+#if !ENABLE_SERIAL_OUT
+    showStats();
+#endif
+
+    executeCommand();
+
+    int waitTime = MIN_LOOP_TIME - (millis() - ts);
+    if (waitTime > 0) delay(waitTime);
+}
