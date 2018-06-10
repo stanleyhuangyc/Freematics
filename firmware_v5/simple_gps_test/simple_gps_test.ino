5512d0e6a428fb10502cb0a532b2de435c5dba7d
Stanley Huang <stanleyhuangyc@gmail.com>
2018-06-10 22:14:32 +1000

    Optimize software serial implementation

--- firmware_v5/simple_gps_test/simple_gps_test.ino
+++ firmware_v5/simple_gps_test/simple_gps_test.ino
@@ -15,6 +15,7 @@
 
 #include <Arduino.h>
 #include <TinyGPS.h>
+#include "esp_task_wdt.h"
 
 #define PIN_LED 4
 #define PIN_GPS_POWER 15
@@ -33,6 +34,8 @@ TaskHandle_t xThread;
 
 unsigned int updates = 0;
 
+#if USE_SOFT_SERIAL
+
 uint32_t inline IRAM_ATTR getCycleCount()
 {
   uint32_t ccount;
@@ -40,24 +43,36 @@ uint32_t inline IRAM_ATTR getCycleCount()
   return ccount;
 }
 
+uint8_t inline IRAM_ATTR readRxPin()
+{
+#if PIN_GPS_UART_RXD < 32
+  return (uint8_t)(GPIO.in >> PIN_GPS_UART_RXD) << 7;
+#else
+  return (uint8_t)(GPIO.in1.val >> (PIN_GPS_UART_RXD - 32)) << 7;
+#endif
+}
+
+#endif
+
 void IRAM_ATTR taskGPS(void* arg)
 {
 #if USE_SOFT_SERIAL
   portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
   for (;;) {
-    while (digitalRead(PIN_GPS_UART_RXD) == HIGH);
+    while (readRxPin());
     uint32_t start = getCycleCount();
-    byte b = 0;
+    uint8_t c = 0;
     taskYIELD();
     taskENTER_CRITICAL(&mux);
-    for (int i = 1; i <= 7; i++) {
-      while (getCycleCount() - start < F_CPU / GPS_BAUDRATE / 2 + (uint32_t)F_CPU * i / GPS_BAUDRATE);
-      if (digitalRead(PIN_GPS_UART_RXD)) b |= 0x80;
-      b >>= 1;
+    for (uint32_t i = 1; i <= 7; i++) {
+      while (getCycleCount() - start < i * F_CPU / GPS_BAUDRATE + F_CPU / GPS_BAUDRATE / 3);
+      c = (c | readRxPin()) >> 1;
     }
     taskEXIT_CRITICAL(&mux);
-    if (gps.encode(b)) updates++;
-    while (getCycleCount() - start < F_CPU / GPS_BAUDRATE / 2 +  (uint32_t)F_CPU * 9 / GPS_BAUDRATE) taskYIELD();
+    if (c && gps.encode(c)) {
+      updates++;
+    }
+    while (getCycleCount() - start < (uint32_t)9 * F_CPU / GPS_BAUDRATE + F_CPU / GPS_BAUDRATE / 2) taskYIELD();
   }
 #else
   for (;;) {
@@ -86,20 +101,22 @@ void setup()
   pinMode(PIN_GPS_POWER, OUTPUT);
   digitalWrite(PIN_GPS_POWER, HIGH);
   // start GPS decoding task
-  xTaskCreatePinnedToCore(taskGPS, "taskGPS", 1024, 0, 1, &xThread, 1);
+  xTaskCreate(taskGPS, "taskGPS", 1024, 0, 1, &xThread);
   Serial.println("Wait for GPS signal...");
   digitalWrite(PIN_LED, LOW);
+  // set watchdog
+  esp_task_wdt_init(3600, 0);
 }
 
 void loop()
 {
   if (updates) {
-    digitalWrite(PIN_LED, HIGH);
     updates = 0;
     static unsigned long lastutc = 0;
     unsigned long utcdate, utctime;
     gps.get_datetime(&utcdate, &utctime, 0);
     if (utctime != lastutc) {
+      digitalWrite(PIN_LED, HIGH);
       lastutc = utctime;
       long lat, lng;
       gps.get_datetime(&utcdate, &utctime, 0);
@@ -119,8 +136,23 @@ void loop()
       Serial.print(alt);
       Serial.print("m Sats:");
       Serial.println(sats);
+#if USE_SOFT_SERIAL
+      static unsigned long start = 0;
+      unsigned long chars;
+      unsigned short good, bad;
+      gps.stats(&chars, &good, &bad);
+      if (good == 0) start = millis() - 1;
+      Serial.print("Stats: ");
+      Serial.print(chars);
+      Serial.print(' ');
+      Serial.print(good);
+      Serial.print(' ');
+      Serial.print((float)(good * 1000) / (millis() - start), 1);
+      Serial.print(' ');
+      Serial.println(bad);
+#endif
+      digitalWrite(PIN_LED, LOW);
     }
-    digitalWrite(PIN_LED, LOW);
   }
-  delay(100);
+  delay(50);
 }
