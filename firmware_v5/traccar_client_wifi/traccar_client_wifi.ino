b7c0ab46270e7480239fbaab3abf9a81f871d72b
Stanley Huang <stanleyhuangyc@gmail.com>
2018-06-27 11:35:49 +1000

    Traccar client over WiFi

--- firmware_v5/traccar_client_wifi/traccar_client_wifi.ino
+++ firmware_v5/traccar_client_wifi/traccar_client_wifi.ino
@@ -16,7 +16,6 @@
 #include <Arduino.h>
 #include <TinyGPS.h>
 #include <WiFi.h>
-#include "driver/gpio.h"
 
 #define PIN_LED 4
 #define PIN_GPS_POWER 15
@@ -24,139 +23,158 @@
 #define PIN_GPS_UART_TXD 33
 
 #define GPS_BAUDRATE 115200
-#define WIFI_SSID "FREEMATICS"
-#define WIFI_PASSWORD "862150909018"
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+#define WIFI_JOIN_INTERVAL 15000
 
+#define TRACCAR_HOST "YOUR_TRACCAR_HOST"
 #define TRACCAR_PORT 5055
-#define TRACCAR_HOST "car.freematics.com"
-#define TRACCAR_DEV_ID "SIMULATOR"
+#define TRACCAR_DEV_ID "YOUR_DEVICE_ID"
 
 HardwareSerial Serial1(1);
 TinyGPS gps;
 WiFiClient client;
 
-#define GPIO_OUTPUT_IO_0    15
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
+bool checkWiFi()
+{
+  // check WiFi connection and re-connect if disconnected
+  static uint32_t wifiStartTime = 0;
+  if (WiFi.status() != WL_CONNECTED) {
+    if (wifiStartTime == 0 || millis() - wifiStartTime > WIFI_JOIN_INTERVAL) {
+        Serial.println();
+        Serial.print("Connecting to WiFi - SSID:");
+        Serial.println(WIFI_SSID);
+        WiFi.disconnect(false);
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        wifiStartTime = millis();
+    }
+    return false;
+  } else {
+    if (wifiStartTime) {
+      // just connected
+      Serial.println();
+      Serial.print("WiFi connected. IP:");
+      Serial.println(WiFi.localIP());
+      wifiStartTime = 0;
+    }
+    return true;
+  }
+}
 
 void setup()
 {
   pinMode(PIN_LED, OUTPUT);
+  // turn on indicator LED
   digitalWrite(PIN_LED, HIGH);
-  delay(100);
+  // turn on GPS power
+  pinMode(PIN_GPS_POWER, OUTPUT);
+  digitalWrite(PIN_GPS_POWER, HIGH);
+
   // USB serial
   Serial.begin(115200);
+
+  delay(500);
   Serial.println("TRACCAR CLIENT");
 
-/*
-  Serial.print("Connecting WiFi");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(500);
-  }
-  Serial.println("OK");
-*/
-
-  digitalWrite(PIN_LED, LOW);
-
-  // turn on GPS power
-  //pinMode(PIN_GPS_POWER, OUTPUT);
-  //digitalWrite(PIN_GPS_POWER, HIGH);
-
-    gpio_config_t io_conf;
-    //disable interrupt
-    io_conf.intr_type = (gpio_int_type_t)GPIO_PIN_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT_OD;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = (gpio_pulldown_t)0;
-    //disable pull-up mode
-    io_conf.pull_up_en = (gpio_pullup_t)0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-    gpio_set_level((gpio_num_t)GPIO_OUTPUT_IO_0, 1);
+  checkWiFi();
 
   // GPS serial
-  //Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, PIN_GPS_UART_RXD, PIN_GPS_UART_TXD);
-  Serial.println("Waiting for GPS signal...");
-  delay(1000);
+  Serial1.begin(GPS_BAUDRATE, SERIAL_8N1, PIN_GPS_UART_RXD, PIN_GPS_UART_TXD);
+  Serial.print("Waiting for GPS signal ");
+
+  // turn off indicator LED
+  digitalWrite(PIN_LED, LOW);
 }
 
 void loop()
 {
+  static unsigned long lastutc = 0;
+
   if (!Serial1.available()) {
     return;
   }
 
   char c = Serial1.read();
-  // output NMEA
-  Serial.print(c);
+  if (lastutc == 0) {
+    // display progress before GPS has signal
+    if (c == '\n') Serial.write('.');
+  }
 
+  // decode NMEA stream
   if (!gps.encode(c)) {
     return;
   }
 
-  if (!client.connected()) {
-    Serial.print("Connecting to ");
-    Serial.print(TRACCAR_HOST);
-    Serial.print("...");
-    if (client.connect(TRACCAR_HOST, TRACCAR_PORT)) {
-      Serial.println("OK");
-    } else {
-      Serial.println("failed");
-      delay(3000);
-      return;
+  if (checkWiFi()) {
+    if (!client.connected()) {
+      Serial.print("Connecting to ");
+      Serial.print(TRACCAR_HOST);
+      Serial.print("...");
+      if (client.connect(TRACCAR_HOST, TRACCAR_PORT)) {
+        Serial.println("OK");
+      } else {
+        Serial.println("failed");
+        delay(3000);
+        return;
+      }
     }
   }
 
-  // new GPS data available
-  static unsigned long lastutc = 0;
   unsigned long utcdate, utctime;
   gps.get_datetime(&utcdate, &utctime, 0);
-  if (utctime != lastutc) {
-    digitalWrite(PIN_LED, HIGH);
-    lastutc = utctime;
-    long lat, lng;
-    gps.get_datetime(&utcdate, &utctime, 0);
-    gps.get_position(&lat, &lng, 0);
-    long speed = gps.speed(); // km/h
-    long alt = gps.altitude(); // m
-    int sats = gps.satellites();
-    Serial.print("UTC:");
-    Serial.print(utctime);
-    Serial.print(" LAT:");
-    Serial.print((float)lat / 1000000, 6);
-    Serial.print(" LNG:");
-    Serial.print((float)lng / 1000000, 6);
-    Serial.print(" Speed:");
-    Serial.print((float)speed * 1852 / 100000, 1);
-    Serial.print("km/h Alt:");
-    Serial.print(alt / 100);
-    Serial.print("m Sats:");
-    Serial.println(sats);
-
-    // generate ISO time string
-    char isotime[256];
-    sprintf(isotime, "%04u-%02u-%02u'T'%02u:%02u:%02u.%u'Z'",
-      (unsigned int)(utcdate % 10000), (unsigned int)(utcdate / 10000) % 100, (unsigned int)(utcdate / 1000000),
-      (unsigned int)(utctime / 1000000), (unsigned int)(utctime % 1000000) / 10000, (unsigned int)(utctime % 10000) / 100, (unsigned int)utctime % 100);
-    Serial.println(isotime);
-
-    // send data in OsmAnd protocol
-    // refer to https://www.traccar.org/osmand
-    client.print(String("GET /id=") + TRACCAR_DEV_ID +
-      "&lat=" + (float)lat / 1000000 + "&lon=" + (float)lng / 1000000 +
-      "&timestamp=" + isotime + "&altitude=" + alt + "&speed=" + speed +
-       " HTTP/1.1\r\n" +
-      "Host: " + TRACCAR_HOST + "\r\n" +
-      "Connection: keep-alive\r\n\r\n");
-
-    while(client.available()) {
-      Serial.print(client.read());
-    }
-
-    digitalWrite(PIN_LED, LOW);
+  if (utctime == lastutc) {
+    return;
   }
+
+  // turn off indicator LED
+  digitalWrite(PIN_LED, HIGH);
+
+  // now that new GPS data available
+  long lat, lng;
+  gps.get_position(&lat, &lng, 0);
+  long speed = gps.speed();
+  long alt = gps.altitude();
+  int sats = gps.satellites();
+  // generate ISO time string
+  char isotime[32];
+  sprintf(isotime, "%04u-%02u-%02uT%02u:%02u:%02u.%01uZ",
+    (unsigned int)(utcdate % 100) + 2000, (unsigned int)(utcdate / 100) % 100, (unsigned int)(utcdate / 10000),
+    (unsigned int)(utctime / 1000000), (unsigned int)(utctime % 1000000) / 10000, (unsigned int)(utctime % 10000) / 100, ((unsigned int)utctime % 100) / 10);
+
+  Serial.print(isotime);
+  Serial.print(" LAT:");
+  Serial.print((float)lat / 1000000, 6);
+  Serial.print(" LNG:");
+  Serial.print((float)lng / 1000000, 6);
+  Serial.print(" Speed:");
+  Serial.print((int)speed * 1852 / 100000);
+  Serial.print("km/h Alt:");
+  Serial.print(alt / 100);
+  Serial.print("m Sats:");
+  Serial.println(sats);
+
+  // arrange and send data in OsmAnd protocol
+  // refer to https://www.traccar.org/osmand
+  char data[128];
+  sprintf(data, "&lat=%f&lon=%f&altitude=%f&speed=%f", (float)lat / 1000000, (float)lng / 1000000, (float)alt / 100, (float)speed / 1000);
+  // send data
+  client.print(String("GET /?id=") + TRACCAR_DEV_ID + "&timestamp=" + isotime + data + " HTTP/1.1\r\n" +
+    "Connection: keep-alive\r\n\r\n");
+
+  // waiting for server response while still decoding NMEA
+  for (uint32_t t = millis(); !client.available() && millis() - t < 3000; ) {
+    if (Serial1.available()) {
+      gps.encode(Serial1.read());
+    }
+  }
+  // output server response
+  while(client.available()) {
+    Serial.print((char)client.read());
+  }
+
+  lastutc = utctime;
+
+  // turn off indicator LED
+  digitalWrite(PIN_LED, LOW);
 }
