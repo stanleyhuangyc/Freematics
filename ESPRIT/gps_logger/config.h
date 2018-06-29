a586d05cc3cb6e676081f4600ff5ddb94173a0a5
Stanley Huang <stanleyhuangyc@gmail.com>
2018-06-29 23:27:52 +1000

    Add traccar client

--- ESPRIT/gps_logger/config.h
+++ ESPRIT/gps_logger/config.h
@@ -5,13 +5,6 @@
 #define STORAGE_SD 1
 #define STORAGE_SPIFFS 2
 
-/**************************************
-* Rate/speed
-**************************************/
-#define GPS_SERIAL_BAUDRATE 115200L
-#define WIFI_JOIN_TIMEOUT 30000
-#define MIN_LOOP_TIME 200
-
 #ifndef HAVE_CONFIG
 
 /**************************************
@@ -23,16 +16,34 @@
 #define ENABLE_SERIAL_OUT 0
 // specify storage type
 #define STORAGE STORAGE_SPIFFS
+// enable(1)/disable(0) screen display
+#define ENABLE_DISPLAY 1
 
 /**************************************
-* WIFI and HTTP server
+* WiFi and HTTP server
 **************************************/
 #define ENABLE_HTTPD 1
 #define ENABLE_WIFI_AP 1
 #define ENABLE_WIFI_STATION 1
 #define WIFI_AP_SSID "GPSLOGGER"
 #define WIFI_AP_PASSWORD "PASSWORD"
-#define WIFI_SSID "HOTSPOT"
-#define WIFI_PASSWORD "..."
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+/**************************************
+* Traccar client
+**************************************/
+#define ENABLE_TRACCAR_CLIENT 1
+#define TRACCAR_HOST "YOUR_TRACCAR_HOST"
+#define TRACCAR_DEV_ID "YOUR_TRACCAR_ID"
 
 #endif
+
+/**************************************
+* Misc
+**************************************/
+#define GPS_SERIAL_BAUDRATE 115200L
+#define WIFI_JOIN_TIMEOUT 30000 /* ms */
+#define MIN_LOOP_TIME 200 /* ms */
+#define TRACCAR_PORT 5055
+#define TRACCAR_SERVER_TIMEOUT 10000
