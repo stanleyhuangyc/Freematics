bd66ac6712776d5e0227de8c853da9bbd2e54cfd
Stanley Huang <stanleyhuangyc@gmail.com>
2018-07-23 13:56:44 +1000

    Modify stats()

--- libraries/TinyGPS/TinyGPS.h
+++ libraries/TinyGPS/TinyGPS.h
@@ -31,7 +31,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define _GPS_KMPH_PER_KNOT 1.852
 #define _GPS_MILES_PER_METER 0.00062137112
 #define _GPS_KM_PER_METER 0.001
-#define _GPS_NO_STATS
 
 class TinyGPS
 {
@@ -88,12 +87,7 @@ public:
   static const char *cardinal(float course);
 
 #ifndef _GPS_NO_STATS
-  void stats(unsigned long *chars, unsigned short *good_sentences, unsigned short *failed_cs);
-  // statistics
-  unsigned long _encoded_characters;
-  unsigned short _good_sentences;
-  unsigned short _failed_checksum;
-  unsigned short _passed_checksum;
+  void stats(unsigned short *good_sentences, unsigned short *failed_cs);
 #endif
 
 private:
@@ -121,6 +115,13 @@ private:
   byte _term_number;
   byte _term_offset;
   bool _gps_data_good;
+
+  // statistics
+#ifndef _GPS_NO_STATS
+  // statistics
+  unsigned short _good_sentences;
+  unsigned short _failed_checksum;
+#endif
 
   // internal utilities
   byte from_hex(char a);
