93434c80d0670f25278a61e003b208295d269651
Stanley Huang <stanleyhuangyc@gmail.com>
2019-04-02 10:31:36 +1100

    Update can_sniffer.ino

--- firmware_v5/can_sniffer/can_sniffer.ino
+++ firmware_v5/can_sniffer/can_sniffer.ino
@@ -23,20 +23,20 @@ void setup()
   // USB serial
   Serial.begin(115200);
 
+  // initialize co-processor link
   while (!sys.begin());
   Serial.print("Firmware: V");
   Serial.println(sys.version);
+  // initialize OBD library
   obd.begin(sys.link);
 
-  Serial.println("Initializing...");
-  
   // stop sniffing in case previously running
   obd.sniff(false);
 
-  // initilize on 11-bit/500Kbps CAN bus
+  // start on 11-bit/500Kbps CAN bus
   while (!obd.init(PROTO_CAN_11B_500K));
   
-  // we are interested in CAN header of 7E*
+  // we are interested in CAN messages with header 7E*
   obd.setHeaderFilter(0x7E0);
   obd.setHeaderMask(0xFFFFFFF0);
 
@@ -47,11 +47,14 @@ void setup()
 void loop()
 {
   byte buf[128];
+  // load one received CAN message into buffer
   int bytes = obd.receiveCAN(buf, sizeof(buf));
   if (bytes > 0) {
+    // print timestamp
     Serial.print('[');
     Serial.print(millis());
     Serial.print("] ");
+    // print received CAN message
     for (int n = 0; n < bytes; n++) {
       byte d = buf[n];
       if (d < 0x10) Serial.print('0');
