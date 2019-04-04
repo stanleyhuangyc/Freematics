575f01d8838d0d4bf8402b03d40dff80b4e8f4d3
Stanley Huang <stanleyhuangyc@gmail.com>
2019-04-04 21:30:11 +1100

    Simulator code moved into server directory

--- server/simulator/config.js
+++
@@ -1,8 +0,0 @@
-const REQUEST_INTERVAL = 1000;
-const ROLLBACK_TIME = 300000;
-const MAX_CACHE_SIZE = 10000;
-const DATA_FETCH_INTERVAL = 500;
-const DATA_SLIDE_INTERVAL = 5000;
-const TRIP_TIMEOUT = 60000;
-
-const serverURL = window.require ? "http://localhost:8080/api/" : "api/";
