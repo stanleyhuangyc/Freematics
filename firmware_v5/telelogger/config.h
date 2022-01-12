3f751f525f47ec217bb561d9888497dd5bb7a264
Andrew Yong <me@ndoo.sg>
2022-01-12 14:55:06 +0800

    telelogger v5: Increase buffer length to accommodate new PIDs

--- firmware_v5/telelogger/config.h
+++ firmware_v5/telelogger/config.h
@@ -5,7 +5,7 @@
 * Circular Buffer Configuration
 **************************************/
 #define BUFFER_SLOTS 32 /* max number of buffer */
-#define BUFFER_LENGTH 128 /* bytes per slot */
+#define BUFFER_LENGTH 256 /* bytes per slot */
 #define SERIALIZE_BUFFER_SIZE 1024 /* bytes */
 
 /**************************************
